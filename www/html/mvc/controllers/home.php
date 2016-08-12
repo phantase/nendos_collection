183a85923bd00cc0ae867258481098d2f16c0b6e
François Tertre <f.tertre@brgm.fr>
2016-08-12 17:16:07 +0200

    Correction of the call to count_allNendoroids after modification of the name of the function

--- www/html/mvc/controllers/home.php
+++ www/html/mvc/controllers/home.php
@@ -1,7 +1,7 @@
 <?php
 
 include_once('mvc/models/get_nendoroids.php');
-$count_nendoroids = count_nendoroids();
+$count_nendoroids = count_allNendoroids();
 $nendoroids = get_allNendoroids();
 foreach ($nendoroids as $key => $nendoroid) {
   $nendoroids[$key]['url'] = preg_replace('/^-+|-+$/', '', strtolower(preg_replace('/[^a-zA-Z0-9]+/', '-', $nendoroid['name'])));
