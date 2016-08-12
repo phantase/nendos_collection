9c364941bcd05fb3f2628464c162b45ec98295a4
François Tertre <f.tertre@brgm.fr>
2016-08-12 17:00:57 +0200

    Correction of request with the new DB structure

--- www/html/mvc/models/get_nendoroids.php
+++ www/html/mvc/models/get_nendoroids.php
@@ -26,7 +26,7 @@ function get_singleNendoroid($number)
 {
   global $bdd;
 
-  $req = $bdd->prepare("SELECT n.internalid, n.boxid, n.name, n.origin, n.version, n.editor, n.dominant_color, b.name AS boxname, b.type AS boxtype FROM nendoroids AS n, boxes AS b WHERE n.boxid=b.internalid AND n.internalid = :number");
+  $req = $bdd->prepare("SELECT n.internalid, n.box_id, n.name, n.origin, n.version, n.editor, n.dominant_color, b.name AS box_name, b.type AS box_type FROM nendoroids AS n, boxes AS b WHERE n.box_id=b.internalid AND n.internalid = :number");
   $req->bindParam(':number',$number);
   $req->execute();
   $nendoroid = $req->fetch();
