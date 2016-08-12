30d6bc21162c6a73cc3ca7a1b1a17a9c752217d5
François Tertre <f.tertre@brgm.fr>
2016-08-12 17:35:02 +0200

    Correction of request for Boxes and addition of new request to retrieve all the boxes

--- www/html/mvc/models/get_boxes.php
+++ www/html/mvc/models/get_boxes.php
@@ -1,12 +1,23 @@
 <?php
-
+/** Count all the boxes available in the DB */
 function count_allBoxes()
 {
   global $bdd;
 
-  $req = $bdd->prepare("SELECT count(*) As count FROM boxes AS a");
+  $req = $bdd->prepare("SELECT count(*) As count FROM boxes AS b");
   $req->execute();
   $count = $req->fetch();
 
   return $count['count'];
+}
+/** Get all the boxes available in the DB */
+function get_allBoxes()
+{
+  global $bdd;
+
+  $req = $bdd->prepare("SELECT b.internalid, b.name, b.type FROM boxes AS b");
+  $req->execute();
+  $boxes = $req->fetchAll(PDO::FETCH_ASSOC);
+
+  return $boxes;
 }
