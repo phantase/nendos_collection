852f7a43ad94c16d28892ada896c97f9e2fa9f5d
François Tertre <f.tertre@brgm.fr>
2016-08-12 17:37:02 +0200

    Correction of requests for Faces

--- www/html/mvc/models/get_faces.php
+++ www/html/mvc/models/get_faces.php
@@ -4,7 +4,7 @@ function count_allFaces()
 {
   global $bdd;
 
-  $req = $bdd->prepare("SELECT count(*) As count FROM faces AS f");
+  $req = $bdd->prepare("SELECT count(*) AS count FROM faces AS f");
   $req->execute();
   $count = $req->fetch();
 
@@ -22,24 +22,24 @@ function get_allFaces()
   return $faces;
 }
 /** Count all the faces available for a specific Nendoroid */
-function count_nendoroidFaces($number)
+function count_nendoroidFaces($nendoroid_id)
 {
   global $bdd;
 
-  $req = $bdd->prepare("SELECT count(*) As count FROM faces AS f WHERE f.nendoroid_id = :number");
-  $req->bindParam(':number',$number);
+  $req = $bdd->prepare("SELECT count(*) AS count FROM faces AS f WHERE f.nendoroid_id = :nendoroid_id");
+  $req->bindParam(':nendoroid_id',$nendoroid_id);
   $req->execute();
   $count = $req->fetch();
 
   return $count['count'];
 }
 /** Get all the faces available for a specific Nendoroid */
-function get_nendoroidFaces($number)
+function get_nendoroidFaces($nendoroid_id)
 {
   global $bdd;
 
-  $req = $bdd->prepare("SELECT f.internalid, f.box_id, f.nendoroid_id, f.eyes, f.eyes_color, f.eyes_color_hex, f.mouth, f.skin_color, f.skin_color_hex, f.sex FROM faces AS f WHERE f.nendoroid_id = :number");
-  $req->bindParam(':number',$number);
+  $req = $bdd->prepare("SELECT f.internalid, f.box_id, f.nendoroid_id, f.eyes, f.eyes_color, f.eyes_color_hex, f.mouth, f.skin_color, f.skin_color_hex, f.sex FROM faces AS f WHERE f.nendoroid_id = :nendoroid_id");
+  $req->bindParam(':nendoroid_id',$nendoroid_id);
   $req->execute();
   $faces = $req->fetchAll(PDO::FETCH_ASSOC);
 
