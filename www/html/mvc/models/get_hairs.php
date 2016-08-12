ea45763dd8e8518dc5240a7a97cdf735ffef5ce8
François Tertre <f.tertre@brgm.fr>
2016-08-12 17:44:48 +0200

    Correction and addition of request for Hairs

--- www/html/mvc/models/get_hairs.php
+++ www/html/mvc/models/get_hairs.php
@@ -1,22 +1,45 @@
 <?php
-
+/** Count all the Hairs available in the DB */
 function count_allHairs()
 {
   global $bdd;
 
-  $req = $bdd->prepare("SELECT count(*) As count FROM hairs AS h");
+  $req = $bdd->prepare("SELECT count(*) AS count FROM hairs AS h");
   $req->execute();
   $count = $req->fetch();
 
   return $count['count'];
 }
-
-function get_nendoroidHairs($number)
+/** Get all the Hairs available in the DB */
+function get_allHairs()
 {
   global $bdd;
 
-  $req = $bdd->prepare("SELECT h.internalid, h.nendoroid_number, h.main_color, h.main_color_hex, h.other_color, h.other_color_hex, h.haircut, h.description, h.frontback FROM hairs AS h WHERE h.nendoroid_number = :number");
-  $req->bindParam(':number',$number);
+  $req = $bdd->prepare("SELECT h.internalid, h.box_id, h.nendoroid_id, h.main_color, h.main_color_hex, h.other_color, h.other_color_hex, h.haircut, h.description, h.frontback FROM hairs AS h");
+  $req->execute();
+  $hairs = $req->fetchAll(PDO::FETCH_ASSOC);
+
+  return $hairs;
+}
+/** Count all the Hairs for a specific Nendoroid */
+function count_nendoroidHairs($nendoroid_id)
+{
+  global $bdd;
+
+  $req = $bdd->prepare("SELECT count(*) AS count FROM hairs AS h WHERE h.nendoroid_id = :nendoroid_id");
+  $req->bindParam(':nendoroid_id',$nendoroid_id);
+  $req->execute();
+  $count = $req->fetch();
+
+  return $count['count'];
+}
+/** Get all the Hairs available for a specific Nendoroid */
+function get_nendoroidHairs($nendoroid_id)
+{
+  global $bdd;
+
+  $req = $bdd->prepare("SELECT h.internalid, h.box_id, h.nendoroid_id, h.main_color, h.main_color_hex, h.other_color, h.other_color_hex, h.haircut, h.description, h.frontback FROM hairs AS h WHERE h.nendoroid_id = :nendoroid_id");
+  $req->bindParam(':nendoroid_id',$nendoroid_id);
   $req->execute();
   $hairs = $req->fetchAll(PDO::FETCH_ASSOC);
 
