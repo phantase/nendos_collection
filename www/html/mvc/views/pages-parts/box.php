248b8b2f53ee09e983744d2d623874b4914deada
François Tertre <f.tertre@brgm.fr>
2016-08-14 12:04:48 +0200

    Modification of CSS to be able to visualize tile by fourth

--- www/html/mvc/views/pages-parts/box.php
+++ www/html/mvc/views/pages-parts/box.php
@@ -5,23 +5,30 @@
                 <h1>BOX: <?= $box['type'] ?> #<?= $box['name'] ?></h1>
               </header>
 
-              <h4>Nendoroids</h4>
-<?php include("mvc/views/pages-sections/nendoroids.php"); ?>
-
+              <div class="row">
+                <div  class="4u">
+                  <span class="image fit">
+                    <img src="images/nendos/boxes/<?= $box['internalid'] ?>.jpg" alt="" />
+                  </span>
+                </div>
+                <div  class="8u">
+                  <h4>Nendoroids</h4>
+<?php
+  $divider = "fourth";
+  include("mvc/views/pages-sections/nendoroids.php");
+?>
               <h4>Faces</h4>
 <?php include("mvc/views/pages-sections/faces.php"); ?>
-
               <h4>Hands</h4>
 <?php include("mvc/views/pages-sections/hands.php"); ?>
-
               <h4>Body Parts</h4>
 <?php include("mvc/views/pages-sections/bodyparts.php"); ?>
-
               <h4>Hairs</h4>
 <?php include("mvc/views/pages-sections/hairs.php"); ?>
-
               <h4>Accessories</h4>
 <?php include("mvc/views/pages-sections/accessories.php"); ?>
+                </div>
+              </div>
 
             </div>
           </div>
--- www/html/mvc/views/pages-sections/nendoroids.php
+++ www/html/mvc/views/pages-sections/nendoroids.php
@@ -1,12 +1,20 @@
               <section class="tiles">
 <?php
 foreach ($nendoroids as $nendoroid) {
+if( isset($divider) ){
 ?>
-                <article class="sixth">
+                <article class="<?= $divider ?>">
+<?php
+} else {
+?>
+                <article>
+<?php
+}
+?>
                   <span class="image fit">
                     <img src="images/nendos/nendoroids/tile-<?= $nendoroid['internalid'] ?>.jpg" alt="" />
                   </span>
-                  <a href="nendoroid/<?= $nendoroid['url'] ?>_<?= $nendoroid['internalid'] ?>/">
+                  <a href="nendoroid/<?= $nendoroid['box_name'] ?>/<?= $nendoroid['url'] ?>_<?= $nendoroid['internalid'] ?>/">
                     <div class="content">
                       <h4><?= $nendoroid['name'] ?></h4>
                     </div>
