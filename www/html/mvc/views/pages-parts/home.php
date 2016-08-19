a7f6fd0544eb48368ede5bcc08b8573cbf53bfbb
François Tertre <f.tertre@brgm.fr>
2016-08-19 16:47:59 +0200

    Modification of Home page to be more responsive

--- www/html/mvc/views/pages-parts/home.php
+++ www/html/mvc/views/pages-parts/home.php
@@ -3,7 +3,7 @@
             <div class="inner">
               <header>
                 <div class="row">
-                  <div class="4u">
+                  <div class="6u 12u$(medium)">
 
                     <?php
                     if(!isset($_SESSION['userid'])) {
@@ -13,18 +13,15 @@
                     }
                     ?>
                   </div>
-                  <div class="2u">
-                    &nbsp;
-                  </div>
-                  <div class="3u"><?php showBoxesCounter($count_boxes); ?></div>
-                  <div class="3u"><?php showNendoroidsCounter($count_nendoroids); ?></div>
+                  <div class="3u 6u(medium)"><?php showBoxesCounter($count_boxes); ?></div>
+                  <div class="3u 6u(medium)"><?php showNendoroidsCounter($count_nendoroids); ?></div>
                 </div>
                 <div class="row">
-                  <div class="2u"><?php showFacesCounter($count_faces); ?></div>
-                  <div class="2u"><?php showHairsCounter($count_hairs); ?></div>
-                  <div class="2u"><?php showHandsCounter($count_hands); ?></div>
-                  <div class="3u"><?php showBodyPartsCounter($count_bodyparts); ?></div>
-                  <div class="3u"><?php showAccessoriesCounter($count_accessories); ?></div>
+                  <div class="2u 6u(medium)"><?php showFacesCounter($count_faces); ?></div>
+                  <div class="2u 6u(medium)"><?php showHairsCounter($count_hairs); ?></div>
+                  <div class="2u 6u(medium)"><?php showHandsCounter($count_hands); ?></div>
+                  <div class="3u 6u(medium)"><?php showBodyPartsCounter($count_bodyparts); ?></div>
+                  <div class="3u 6u(medium)"><?php showAccessoriesCounter($count_accessories); ?></div>
                 </div>
               </header>
               <p/>
