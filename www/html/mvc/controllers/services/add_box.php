37eef82f12163966a24cb1ebce20e90baf28fddd
François Tertre <f.tertre@brgm.fr>
2016-08-24 17:38:15 +0200

    Add a poor authorization control on the add box service

--- www/html/mvc/controllers/services/add_box.php
+++ www/html/mvc/controllers/services/add_box.php
@@ -2,6 +2,11 @@
 
 header('Content-Type: application/json');
 
+if( ! isset($_SESSION['userid']) ){
+  echo json_encode(array('result'=>'failure','reason'=>'Not authorized'));
+  exit;
+}
+
 if( isset($_POST['new_box_name']) && strlen($_POST['new_box_name'])>0 && isset($_POST['new_box_type']) && strlen($_POST['new_box_type'])>0 ){
 
   $box_name = $_POST['new_box_name'];
@@ -13,10 +18,10 @@ if( isset($_POST['new_box_name']) && strlen($_POST['new_box_name'])>0 && isset($
   if( isset($boxinternalid) ){
     echo json_encode(array('result'=>'success','box_internalid'=>$boxinternalid,'box_name'=>$box_name,'box_type'=>$box_type));
   } else {
-    echo json_encode(array('result'=>'failure'));
+    echo json_encode(array('result'=>'failure','reason'=>'Not able to create DB record'));
   }
 
 
 } else {
-  echo json_encode(array('result'=>'failure'));
+  echo json_encode(array('result'=>'failure','reason'=>'Bad parameters'));
 }
