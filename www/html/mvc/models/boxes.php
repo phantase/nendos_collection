f1d783a618171ef84b2ae93b0a28202c0fb8935c
Phantase <phantase@free.fr>
2016-08-31 10:24:02 +0200

    Add ordering on get_allBoxes

--- www/html/mvc/models/boxes.php
+++ www/html/mvc/models/boxes.php
@@ -4,18 +4,33 @@ function count_allBoxes()
 {
   global $bdd;
 
-  $req = $bdd->prepare("SELECT count(*) As count FROM boxes AS b");
+  $req = $bdd->prepare("SELECT count(*) As count
+                        FROM boxes AS b");
   $req->execute();
   $count = $req->fetch();
 
   return $count['count'];
 }
 /** Get all the boxes available in the DB */
-function get_allBoxes()
+function get_allBoxes($order="name",$direction="DESC")
 {
+  $orders = array("internalid","name","type","creator_name,creation,editor_name,edition");
+  $key = array_search($order, $orders);
+  $order = $orders[$key];
+  $directions = array("ASC","DESC");
+  $key = array_search($direction, $directions);
+  $direction = $directions[$key];
+
   global $bdd;
 
-  $req = $bdd->prepare("SELECT b.internalid, b.name, b.type FROM boxes AS b");
+  $req = $bdd->prepare("SELECT b.internalid, b.name, b.type,
+                        b.creator, uc.username AS creator_name, b.creation,
+                        b.editor, ue.username AS editor_name, b.edition,
+                        NOW() AS now
+                        FROM boxes AS b
+                        LEFT JOIN users AS uc ON b.creator = uc.internalid
+                        LEFT JOIN users AS ue ON b.creator = ue.internalid
+                        ORDER BY $order $direction");
   $req->execute();
   $boxes = $req->fetchAll(PDO::FETCH_ASSOC);
 
@@ -26,7 +41,14 @@ function get_singleBox($boxinternalid)
 {
   global $bdd;
 
-  $req = $bdd->prepare("SELECT b.internalid, b.name, b.type, b.creator, uc.username AS creator_name, b.creation, b.editor, ue.username AS editor_name, b.edition, NOW() AS now FROM boxes AS b LEFT JOIN users AS uc ON b.creator = uc.internalid LEFT JOIN users AS ue ON b.creator = ue.internalid WHERE b.internalid = :boxinternalid");
+  $req = $bdd->prepare("SELECT b.internalid, b.name, b.type,
+                        b.creator, uc.username AS creator_name, b.creation,
+                        b.editor, ue.username AS editor_name, b.edition,
+                        NOW() AS now
+                        FROM boxes AS b
+                        LEFT JOIN users AS uc ON b.creator = uc.internalid
+                        LEFT JOIN users AS ue ON b.creator = ue.internalid
+                        WHERE b.internalid = :boxinternalid");
   $req->bindParam(':boxinternalid',$boxinternalid);
   $req->execute();
   $box = $req->fetch();
@@ -38,7 +60,8 @@ function add_singleBox($boxname,$boxtype,$userid)
 {
   global $bdd;
 
-  $req = $bdd->prepare("INSERT INTO boxes(name,type,creator,creation,editor,edition) VALUES(:boxname,:boxtype,:creator,NOW(),:editor,NOW())");
+  $req = $bdd->prepare("INSERT INTO boxes(name,type,creator,creation,editor,edition)
+                        VALUES(:boxname,:boxtype,:creator,NOW(),:editor,NOW())");
   $req->bindParam(':boxname',$boxname);
   $req->bindParam(':boxtype',$boxtype);
   $req->bindParam(':creator',$userid);
