9d02d4631d2558cd94a13707dcb8114406cedc35
Phantase <phantase@free.fr>
2016-08-31 23:56:20 +0200

    Correction of a mistake after the rename of editor to company in nendoroids table

--- www/html/mvc/views/pages-sections/article_listings/nendoroids.php
+++ www/html/mvc/views/pages-sections/article_listings/nendoroids.php
@@ -29,7 +29,7 @@ foreach ($nendoroids as $nendoroid) {
 <?php } else  if( $renderer == "home" ) { ?>
                     <h2>#<?= $nendoroid['box_name'] ?><br/><?= $nendoroid['name'] ?></h2>
                     <div class="content">
-                      <p><?= $nendoroid['origin'] ?> - <?= $nendoroid['name'] ?> - <?= $nendoroid['box_type'] ?> #<?= $nendoroid['box_name'] ?> - <?= $nendoroid['version'] ?> (<?= $nendoroid['editor'] ?>)</p>
+                      <p><?= $nendoroid['origin'] ?> - <?= $nendoroid['name'] ?> - <?= $nendoroid['box_type'] ?> #<?= $nendoroid['box_name'] ?> - <?= $nendoroid['version'] ?> (<?= $nendoroid['company'] ?>)</p>
                     </div>
 <?php } ?>
                   </a>
