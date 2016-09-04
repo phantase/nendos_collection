96d3c5a7ce715d6580bec6b6a6f31cb1cf0f4f88
Phantase <phantase@free.fr>
2016-09-04 21:26:48 +0200

    Reactivating single view of hairs

--- www/html/mvc/controllers/single/hair.php
+++ www/html/mvc/controllers/single/hair.php
@@ -1,26 +1,39 @@
 <?php
 
-if( isset($_GET['hairinternalid']) ){
-  $hairinternalid = $_GET['hairinternalid'];
+if( isset($_GET['hair_internalid']) ){
+  $hair_internalid = $_GET['hair_internalid'];
 
-  include_once('mvc/models/hairs.php');
-  $hair = get_singleHair($hairinternalid);
-  if($hair['nendoroid_id']){
-    $hair['nendoroid_url'] = urlize($hair['nendoroid_name']);
+  $resultInfo = get_singleHair($hair_internalid);
+
+  if($resultInfo[0]=="00000"){
+
+    $hair = $resultInfo[4];
+
+    $hair['box_url'] = urlize($hair['box_name']);
+
+    if( isset($hair['nendoroid_name']) && strlen($hair['nendoroid_name'])>0 ){
+      $hair['nendoroid_url'] = urlize($hair['nendoroid_name']);
+    }
+
+    $metadata = array('db_creatorid'    =>  $hair['db_creatorid'],
+                      'db_creatorname'  =>  $hair['db_creatorname'],
+                      'db_creationdate' =>  $hair['db_creationdate'],
+                      'db_creationdiff' =>  ((new DateTime($hair['now']))->diff(new DateTime($hair['db_creationdate']))),
+                      'db_editorid'     =>  $hair['db_editorid'],
+                      'db_editorname'   =>  $hair['db_editorname'],
+                      'db_editiondate'  =>  $hair['db_editiondate'],
+                      'db_editiondiff'  =>  ((new DateTime($hair['now']))->diff(new DateTime($hair['db_editiondate']))));
+
+    $page_title = "hair";
+
+  } else {
+    $include_page = "error";
+    $page_title = "Error";
   }
-  $metadata = array('creator'=>$hair['creator'],
-                    'creator_name'=>$hair['creator_name'],
-                    'creation'=>$hair['creation'],
-                    'creation_diff'=>((new DateTime($hair['now']))->diff(new DateTime($hair['creation']))),
-                    'editor'=>$hair['editor'],
-                    'editor_name'=>$hair['editor_name'],
-                    'edition'=>$hair['edition'],
-                    'edition_diff'=>((new DateTime($hair['now']))->diff(new DateTime($hair['edition']))));
-
-  $page_title = "Hair";
-
-  include_once('mvc/views/pages/skeleton.php');
 
 } else {
-
+  $include_page = "error";
+  $page_title = "Error";
 }
+
+include_once('mvc/views/pages/skeleton.php');
--- www/html/mvc/views/pages-parts/single/hair.php
+++ www/html/mvc/views/pages-parts/single/hair.php
@@ -3,13 +3,19 @@
             <div class="inner">
               <div class="row">
                 <div  class="4u 12u(medium)">
-                  <span class="image fit" id="span_hair<?= $hair['internalid'] ?>">
-                    <img src="images/nendos/hairs/<?= $hair['internalid'] ?>.jpg" alt="" />
+                  <span class="image fit" id="span_hair<?= $hair['hair_internalid'] ?>">
+                    <img src="images/nendos/hairs/<?= $hair['hair_internalid'] ?>.jpg" alt="" />
                     <?php if(isset($_SESSION['userid'])){ ?>
-                      <i class="icon style2 fa-edit included editpic" id="editpic_hair<?= $hair['internalid'] ?>" title="Add/Change the picture" part="hair" internalid="<?= $hair['internalid'] ?>"></i>
+                      <i class="icon style2 fa-edit included editpic"
+                        id="editpic_hair<?= $hair['hair_internalid'] ?>"
+                        title="Add/Change the picture"
+                        part="hair"
+                        internalid="<?= $hair['hair_internalid'] ?>"></i>
                     <?php } ?>
                   </span>
-                  <fieldset class="dropzone image fit" id="drop_hair<?= $hair['internalid'] ?>" style="display:none;">
+                  <fieldset class="dropzone image fit"
+                            id="drop_hair<?= $hair['hair_internalid'] ?>"
+                            style="display:none;">
                     <p>Drop a file here, <br/>or click to browse your computer...</p>
                   </fieldset>
                 </div>
@@ -19,28 +25,37 @@
                         <tbody>
                           <tr>
                             <th>Box</th>
-                            <td colspan="3"><a href="box/<?= $hair['box_name'] ?>_<?= $hair['box_id'] ?>/"><?= $hair['box_type'] ?> #<?= $hair['box_name'] ?></a></td>
+                            <td colspan="3">
+                              <a href="box/<?= $hair['box_internalid'] ?>/<?= $hair['box_url'] ?>/">
+                                <?= $hair['box_category'] ?>
+                                <?php if( isset($hair['box_number']) && strlen($hair['box_number'])>0 ){ ?>
+                                  #<?= $hair['box_number'] ?>
+                                <?php } ?>
+                                <br/>
+                                <?= $hair['box_name'] ?>
+                              </a>
+                            </td>
                           </tr>
-<?php if($hair['nendoroid_id']){ ?>
+<?php if( isset($hair['nendoroid_name']) && strlen($hair['nendoroid_name'])>0 ){ ?>
                           <tr>
                             <th>Nendoroid</th>
-                            <td colspan="3"><a href="nendoroid/<?= $hair['nendoroid_url'] ?>_<?= $hair['nendoroid_id'] ?>/"><?= $hair['nendoroid_name'] ?><?php if(isset($hair['nendoroid_version']) && strlen($hair['nendoroid_version'])>0){ ?> - <?= $hair['nendoroid_version'] ?><?php } ?></a></td>
+                            <td colspan="3"><a href="nendoroid/<?= $hair['nendoroid_internalid'] ?>/<?= $hair['nendoroid_url'] ?>/"><?= $hair['nendoroid_name'] ?><?php if(isset($hair['nendoroid_version']) && strlen($hair['nendoroid_version'])>0){ ?> - <?= $hair['nendoroid_version'] ?><?php } ?></a></td>
                           </tr>
 <?php } ?>
                           <tr>
                             <th>Haircut</th>
-                            <td colspan="2"><?= $hair['haircut'] ?></td>
-                            <td><?= $hair['frontback'] ?></td>
+                            <td colspan="2"><?= $hair['hair_haircut'] ?></td>
+                            <td><?= $hair['hair_frontback'] ?></td>
                           </tr>
                           <tr>
                             <th>Main color</th>
-                            <td><?= $hair['main_color'] ?></td>
+                            <td><?= $hair['hair_main_color'] ?></td>
                             <th>Other color</th>
-                            <td><?= $hair['other_color'] ?></td>
+                            <td><?= $hair['hair_other_color'] ?></td>
                           </tr>
                           <tr>
                             <th>Description</th>
-                            <td colspan="3"><?= $hair['description'] ?></td>
+                            <td colspan="3"><?= $hair['hair_description'] ?></td>
                           </tr>
                         </tbody>
                       </table>
