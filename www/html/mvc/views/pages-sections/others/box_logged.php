e63fef3a017d642e88dbbb28801f9753f051435a
Phantase <phantase@free.fr>
2016-09-15 15:22:43 +0200

    Icons to show the status of the logged user in the home screen

--- www/html/mvc/views/pages-sections/others/box_logged.php
+++ www/html/mvc/views/pages-sections/others/box_logged.php
@@ -4,6 +4,17 @@
                           <div class="info-box-text" id="username_holder">
                             <?= $_SESSION['username'];?>
                             <a id="logout_submit">(Log out)</a>
+                            <span style="float:right;">
+<?php if( isAdministrator() ) { ?>
+                              <i class="icon fa-cogs" title="Administrator"></i>
+<?php } ?>
+<?php if( isValidator() ) { ?>
+                              <i class="icon fa-check-square-o" title="Validator"></i>
+<?php } ?>
+<?php if( isEditor() ) { ?>
+                              <i class="icon fa-pencil-square-o" title="Editor"></i>
+<?php } ?>
+                            </span>
                           </div>
                         </div>
                       </div>
