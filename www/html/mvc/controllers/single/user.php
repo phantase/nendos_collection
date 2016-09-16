0a71018ce1347055c6f9cd436ab5831b7b2d15d7
Phantase <phantase@free.fr>
2016-09-16 14:19:19 +0200

    Now an user can view his own user single page, even if he's not an administrator

--- www/html/mvc/controllers/single/user.php
+++ www/html/mvc/controllers/single/user.php
@@ -1,26 +1,25 @@
 <?php
 
-if( !isAdministrator() ){
+if( !isset($_GET['userid']) ){
+  raiseError("There is no user id provided.");
+}
+
+if( !(isAdministrator() || $_SESSION['userid']==$_GET['userid']) ){
   raiseError("You don't have right to do that.");
 }
 
-if( isset($_GET['userid']) ){
-  $userid = $_GET['userid'];
+$userid = $_GET['userid'];
 
-  $resultInfo = get_singleUser($userid);
+$resultInfo = get_singleUser($userid);
 
-  if($resultInfo[0]=="00000"){
+if($resultInfo[0]=="00000"){
 
-    $user = $resultInfo[4];
+  $user = $resultInfo[4];
 
-    $page_title = "User";
-
-  } else {
-    raiseError($resultInfo[2]);
-  }
+  $page_title = "User";
 
 } else {
-  raiseError("There is no user id provided.");
+  raiseError($resultInfo[2]);
 }
 
 include_once('mvc/views/pages/skeleton.php');
