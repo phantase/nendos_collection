4c0fd58e167862de4db0ec3306c7b8537ce240f9
Phantase <phantase@free.fr>
2016-09-16 14:42:10 +0200

    Remove the add Box .. box to the home screen of non editor

--- www/html/mvc/views/pages-parts/home.php
+++ www/html/mvc/views/pages-parts/home.php
@@ -3,22 +3,23 @@
             <div class="inner">
               <header>
                 <div class="row">
-                  <?php
-                  if(!isset($_SESSION['userid'])) {
-                  ?>
+<?php if( ! isLogged() ) { ?>
                   <div class="6u 12u$(medium)">
                     <?php
                       include('mvc/views/pages-sections/others/box_login.php');
                     ?>
                   </div>
-                  <?php
-                    } else {
-                  ?>
+<?php } else { ?>
+<?php if( isEditor() ) { ?>
                   <div class="4u 8u(medium)">
+<?php } else { ?>
+                  <div class="6u 8u(medium)">
+<?php } ?>
                     <?php
                       include('mvc/views/pages-sections/others/box_logged.php');
                     ?>
                   </div>
+<?php if( isEditor() ) { ?>
                   <div class="2u 4u$(medium)">
                     <a href="addbox">
                       <div class="info-box add-box nendo_boxes_icon" id="button_new_box">
@@ -26,9 +27,8 @@
                       </div>
                     </a>
                   </div>
-                  <?php
-                    }
-                  ?>
+<?php } // End isEditor ?>
+<?php } // End isLogged ?>
                   <div class="3u 6u(medium)"><?php showBoxesCounter($count_boxes); ?></div>
                   <div class="3u 6u(medium)"><?php showNendoroidsCounter($count_nendoroids); ?></div>
                 </div>
