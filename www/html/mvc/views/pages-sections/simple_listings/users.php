c8c793fec8cd471c3a2bb7e82bb3d33f168e480c
Phantase <phantase@free.fr>
2016-09-16 11:14:43 +0200

    Users listing is done

--- www/html/mvc/views/pages-sections/simple_listings/users.php
+++ www/html/mvc/views/pages-sections/simple_listings/users.php
@@ -1,54 +1,47 @@
                   <div class="row">
-                    <div class="12u$">
-                      <div class="table-wrapper">
-                        <table>
-                          <tr>
-                            <th>Id</th>
-                            <th>Username</th>
-                            <th>Mail</th>
-                            <th>Rights</th>
-                            <th>Joined date</th>
-                            <th>Last view on</th>
-                          </tr>
 <?php
   foreach ($users as $user) {
 ?>
-                          <tr>
-                            <td><?= $user['internalid'] ?></td>
-                            <td>
-<?php if($withlinks){ ?>
-                              <a href="user/<?= $user['internalid'] ?>/">
-<?php } ?>
-                                <?= $user['username'] ?>
-<?php if($withlinks){ ?>
-                              </a>
-<?php } ?>
-                            </td>
-                            <td>
-                              <?= $user['usermail'] ?>
-                            </td>
-                            <td>
-<?php if( $user['administrator'] ) { ?>
-                              <i class="icon fa-cogs" title="Administrator"></i>
-<?php } ?>
-<?php if( $user['validator'] ) { ?>
-                              <i class="icon fa-check-square-o" title="Validator"></i>
-<?php } ?>
-<?php if( $user['editor'] ) { ?>
-                              <i class="icon fa-pencil-square-o" title="Editor"></i>
-<?php } ?>
-                            </td>
-                            <td>
-                              <?= $user['signupdate'] ?>
-                            </td>
-                            <td>
-                              <?= $user['lastviewdate'] ?>
-                            </td>
-                          </tr>
+                    <div class="3u 6u(medium) 12u(small)">
+                      <div class="user">
+                        <div>
+                          <span class="userid">
+                            <?= $user['internalid'] ?>
+                          </span>
+                          <span class="username">
+  <?php if($withlinks){ ?>
+                            <a href="user/<?= $user['internalid'] ?>/">
+  <?php } ?>
+                              <?= $user['username'] ?>
+  <?php if($withlinks){ ?>
+                            </a>
+  <?php } ?>
+                          </span>
+                          <span class="righticons">
+  <?php if( $user['administrator'] ) { ?>
+                            <i class="icon fa-cogs" title="Administrator"></i>
+  <?php } ?>
+  <?php if( $user['validator'] ) { ?>
+                            <i class="icon fa-check-square-o" title="Validator"></i>
+  <?php } ?>
+  <?php if( $user['editor'] ) { ?>
+                            <i class="icon fa-pencil-square-o" title="Editor"></i>
+  <?php } ?>
+                            <i class="icon fa-user" title="User"></i>
+                          </span>
+                        </div>
+                        <div>
+                          <?= $user['usermail'] ?>
+                        </div>
+                        <div>
+                          Sign up: <i><?= $user['signupdate'] ?></i>
+                        </div>
+                        <div>
+                          Last on: <i><?= $user['lastviewdate'] ?></i>
+                        </div>
+                      </div>
+                    </div>
 <?php
   }
 ?>
-                        </table>
-                      </div>
-                    </div>
                   </div>
