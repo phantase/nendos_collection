1601fca10c679f96a403c2226b5a1e04f9b66943
Phantase <phantase@free.fr>
2016-09-21 19:03:36 +0200

    Remove icons for the status of the connected user (it was not really useful in fact)

--- www/html/mvc/views/pages-sections/others/box_logged.php
+++ www/html/mvc/views/pages-sections/others/box_logged.php
@@ -4,20 +4,8 @@
                           <div class="info-box-text" id="username_holder">
                             <a href="user/<?= $_SESSION['userid'] ?>/">
                               <?= $_SESSION['username'];?>
-                            </a>
+                            </a><br/>
                             <a id="logout_submit">(Log out)</a>
-                            <span style="float:right;">
-<?php if( isAdministrator() ) { ?>
-                              <i class="icon fa-cogs" title="Administrator"></i>
-<?php } ?>
-<?php if( isValidator() ) { ?>
-                              <i class="icon fa-check-square-o" title="Validator"></i>
-<?php } ?>
-<?php if( isEditor() ) { ?>
-                              <i class="icon fa-pencil-square-o" title="Editor"></i>
-<?php } ?>
-                              <i class="icon fa-user" title="User"></i>
-                            </span>
                           </div>
                         </div>
                       </div>
