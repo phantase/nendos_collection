674c2a95271687d5f277e37a2f65e483a5ab172f
Phantase <phantase@free.fr>
2016-09-21 18:59:39 +0200

    Remove words not useful in the login box of the home screen

--- www/html/mvc/views/pages-sections/others/box_login.php
+++ www/html/mvc/views/pages-sections/others/box_login.php
@@ -2,8 +2,7 @@
                         <span class="info-box-icon login_icon"></span>
                         <div class="info-box-content">
                           <div>
-                            You are not connected.<br/>
-                            Please <a href="login">log in</a> or <a href="signup">sign up</a>
+                            <a href="login">Log in</a> or <a href="signup">Sign up</a>
                           </div>
                         </div>
                       </div>
