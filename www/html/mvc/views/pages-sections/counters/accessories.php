d472da92ef9da00930a57fff51db7fd396fcac0f
Phantase <phantase@free.fr>
2016-09-21 18:55:19 +0200

    Info Box have now their title label dependent of scale

--- www/html/mvc/views/pages-sections/counters/accessories.php
+++ www/html/mvc/views/pages-sections/counters/accessories.php
@@ -2,7 +2,8 @@
                       <div class="info-box">
                         <span class="info-box-icon nendo_accessories_icon"></span>
                         <div class="info-box-content">
-                          <span class="info-box-text">Accessories</span>
+                          <span class="info-box-text mediumu">Access.</span>
+                          <span class="info-box-text larged">Accessories</span>
                           <span class="info-box-number"><?= $count_accessories; ?></span>
                         </div>
                       </div>
