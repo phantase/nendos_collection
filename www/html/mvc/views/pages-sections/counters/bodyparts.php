614ced20e04095651280d6eeaf2ef0df6e794149
Phantase <phantase@free.fr>
2016-09-21 19:23:59 +0200

    Return to the normal for the label of the infobox, but now the size of the icon and the size of the label is dependant of the scale (not marvelous, but not bad too)

--- www/html/mvc/views/pages-sections/counters/bodyparts.php
+++ www/html/mvc/views/pages-sections/counters/bodyparts.php
@@ -2,8 +2,7 @@
                       <div class="info-box">
                         <span class="info-box-icon nendo_body_icon"></span>
                         <div class="info-box-content">
-                          <span class="info-box-text mediumu">B.parts</span>
-                          <span class="info-box-text larged">Body parts</span>
+                          <span class="info-box-text">Body parts</span>
                           <span class="info-box-number"><?= $count_bodyparts; ?></span>
                         </div>
                       </div>
--- www/html/mvc/views/pages-sections/counters/nendoroids.php
+++ www/html/mvc/views/pages-sections/counters/nendoroids.php
@@ -2,8 +2,7 @@
                       <div class="info-box">
                         <span class="info-box-icon nendo_nendo_icon"></span>
                         <div class="info-box-content">
-                          <span class="info-box-text mediumu">Nendo.</span>
-                          <span class="info-box-text larged">Nendoroids</span>
+                          <span class="info-box-text">Nendoroids</span>
                           <span class="info-box-number"><?= $count_nendoroids; ?></span>
                         </div>
                       </div>
