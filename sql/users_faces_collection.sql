bb4887ac0b3416d739941e22a4b9be8f4046373b
Phantase <phantase@free.fr>
2016-09-22 22:15:03 +0200

    Adding the SQL script for the creation of collection table for faces

---
+++ sql/users_faces_collection.sql
@@ -0,0 +1,20 @@
+-- Create the table
+CREATE TABLE `users_faces_collection` (
+  `internalid` bigint(20) UNSIGNED NOT NULL,
+  `userid` bigint(20) UNSIGNED NOT NULL,
+  `faceid` bigint(20) UNSIGNED NOT NULL,
+  `additiondate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- Add the indexes
+ALTER TABLE `users_faces_collection`
+  ADD PRIMARY KEY (`internalid`),
+  ADD UNIQUE KEY `internalid` (`internalid`),
+  ADD KEY `userid` (`userid`),
+  ADD KEY `faceid` (`faceid`);
+-- Add the auto increment
+ALTER TABLE `users_faces_collection`
+  MODIFY `internalid` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+-- Add the constraints
+ALTER TABLE `users_faces_collection`
+  ADD CONSTRAINT `users_faces_collection_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`internalid`),
+  ADD CONSTRAINT `users_faces_collection_ibfk_2` FOREIGN KEY (`faceid`) REFERENCES `faces` (`internalid`);
