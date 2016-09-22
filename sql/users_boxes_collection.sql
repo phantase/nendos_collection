16c93f5a84412354e5f63ea6b33865ab1d5ba571
Phantase <phantase@free.fr>
2016-09-22 20:22:27 +0200

    Adding the SQL scripts for the creation of collection tables (for boxes and nendoroids atm)

---
+++ sql/users_boxes_collection.sql
@@ -0,0 +1,20 @@
+-- Create the table
+CREATE TABLE `users_boxes_collection` (
+  `internalid` bigint(20) UNSIGNED NOT NULL,
+  `userid` bigint(20) UNSIGNED NOT NULL,
+  `boxid` bigint(20) UNSIGNED NOT NULL,
+  `additiondate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- Add the indexes
+ALTER TABLE `users_boxes_collection`
+  ADD PRIMARY KEY (`internalid`),
+  ADD UNIQUE KEY `internalid` (`internalid`),
+  ADD KEY `userid` (`userid`),
+  ADD KEY `boxid` (`boxid`);
+-- Add the auto increment
+ALTER TABLE `users_boxes_collection`
+  MODIFY `internalid` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+-- Add the constraints
+ALTER TABLE `users_boxes_collection`
+  ADD CONSTRAINT `users_boxes_collection_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`internalid`),
+  ADD CONSTRAINT `users_boxes_collection_ibfk_2` FOREIGN KEY (`boxid`) REFERENCES `boxes` (`internalid`);
---
+++ sql/users_nendoroids_collection.sql
@@ -0,0 +1,20 @@
+-- Create the table
+CREATE TABLE `users_nendoroids_collection` (
+  `internalid` bigint(20) UNSIGNED NOT NULL,
+  `userid` bigint(20) UNSIGNED NOT NULL,
+  `nendoroidid` bigint(20) UNSIGNED NOT NULL,
+  `additiondate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- Add the indexes
+ALTER TABLE `users_nendoroids_collection`
+  ADD PRIMARY KEY (`internalid`),
+  ADD UNIQUE KEY `internalid` (`internalid`),
+  ADD KEY `userid` (`userid`),
+  ADD KEY `nendoroidid` (`nendoroidid`);
+-- Add the auto increment
+ALTER TABLE `users_nendoroids_collection`
+  MODIFY `internalid` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+-- Add the constraints
+ALTER TABLE `users_nendoroids_collection`
+  ADD CONSTRAINT `users_nendoroids_collection_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`internalid`),
+  ADD CONSTRAINT `users_nendoroids_collection_ibfk_2` FOREIGN KEY (`nendoroidid`) REFERENCES `nendoroids` (`internalid`);
