7794e4054d3496d2d3b284112726235ef83e245d
Phantase <phantase@free.fr>
2016-11-12 19:11:51 +0100

    Move Menu outside of Skeleton, and add an item to add a new box

---
+++ www/html/mvc/views/pages-sections/others/menu.php
@@ -0,0 +1,22 @@
+        <!-- Menu -->
+          <nav id="menu">
+            <h2>Menu</h2>
+            <ul>
+              <li><a href="./home">Home</a></li>
+<?php if(isEditor()){ ?>
+              <li><a href="./addbox">Add a Box</a></li>
+<?php } ?>
+              <li><a href="./boxes">Boxes</a></li>
+              <li><a href="./nendoroids">Nendoroids</a></li>
+              <li><a href="./faces">Faces</a></li>
+              <li><a href="./hairs">Hairs</a></li>
+              <li><a href="./hands">Hands</a></li>
+              <li><a href="./bodyparts">Body Parts</a></li>
+              <li><a href="./accessories">Accessories</a></li>
+              <li><a href="./credits">Credits</a></li>
+<?php if(isAdministrator()){ ?>
+              <li><a href="./users">Users</a></li>
+              <li><a href="elements.html">Elements</a></li>
+<?php } ?>
+            </ul>
+          </nav>
--- www/html/mvc/views/pages/skeleton.php
+++ www/html/mvc/views/pages/skeleton.php
@@ -54,27 +54,9 @@ if( file_exists('mvc/views/pages-sections/sort/'.$include_page.'.php') ) {
             </div>
           </header>
 
-        <!-- Menu -->
-          <nav id="menu">
-            <h2>Menu</h2>
-            <ul>
-              <li><a href="./home">Home</a></li>
-              <li><a href="./boxes">Boxes</a></li>
-              <li><a href="./nendoroids">Nendoroids</a></li>
-              <li><a href="./faces">Faces</a></li>
-              <li><a href="./hairs">Hairs</a></li>
-              <li><a href="./hands">Hands</a></li>
-              <li><a href="./bodyparts">Body Parts</a></li>
-              <li><a href="./accessories">Accessories</a></li>
-              <li><a href="./credits">Credits</a></li>
-<?php if(isAdministrator()){ ?>
-              <li><a href="./users">Users</a></li>
-              <li><a href="elements.html">Elements</a></li>
-<?php } ?>
-            </ul>
-          </nav>
-
 <?php
+  include_once('mvc/views/pages-sections/others/menu.php');
+
   include_once('mvc/views/pages-parts/'.$include_page.'.php');
 ?>
 
