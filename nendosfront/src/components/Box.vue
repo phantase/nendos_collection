d5c2f1d2741517991f5e8c7bd075c2fbe3ad496c
Phantase <phantase@free.fr>
2017-02-15 22:40:45 +0100

    Change box vue and add a new component : boxheader

--- nendosfront/src/components/Box.vue
+++ nendosfront/src/components/Box.vue
@@ -1,28 +1,99 @@
 <template>
-  <div class="app-box">
-    <div class="box" v-if="box.internalid">
-      <div class="box-header with-border">
-        <h3 class="box-title">
-          <div class="box-category text-yellow">
-            <span>{{ box.category}}</span>
-            <span v-if="box.number">#{{ box.number}}</span>
+  <div class="app-box" v-if="box.internalid">
+
+    <div class="row">
+      <div class="col-md-12">
+        <div class="box">
+          <div class="box-header with-border">
+            <h3 class="box-title">
+              <div class="box-category text-yellow">
+                <span>{{ box.category}}</span>
+                <span v-if="box.number">#{{ box.number}}</span>
+              </div>
+              <div class="box-name">{{ box.name }}</div>
+              <div class="box-series">{{ box.series }}</div>
+            </h3>
           </div>
-          <div class="box-name">{{ box.name }}</div>
-          <div class="box-series">{{ box.series }}</div>
-        </h3>
-      </div>
-      <div class="box-body">
-        <img :src="resources.imagesurl+'/images/nendos/boxes/'+box.internalid+'_thumb'" />
+        </div>
       </div>
     </div>
+
+    <div class="row">
+      <div class="col-md-8 col-sm-12 col-xs-12">
+        <div class="box">
+          <box-header title="Informations" collapsable="true" icon="fa-info"></box-header>
+          <div class="box-body">
+            <ul class="list-group list-group-unbordered">
+              <li class="list-group-item" v-if="box.number">
+                <b>Number</b>
+                <a class="pull-right">{{ box.number }}</a><br>
+              </li>
+              <li class="list-group-item" v-if="box.name">
+                <b>Name</b>
+                <a class="pull-right">{{ box.name }}</a><br>
+              </li>
+              <li class="list-group-item" v-if="box.series">
+                <b>Series</b>
+                <a class="pull-right">{{ box.series }}</a><br>
+              </li>
+              <li class="list-group-item" v-if="box.manufacturer">
+                <b>Manufacturer</b>
+                <a class="pull-right">{{ box.manufacturer }}</a><br>
+              </li>
+              <li class="list-group-item" v-if="box.price">
+                <b>Price ¥</b>
+                <a class="pull-right">{{ box.price }}</a><br>
+              </li>
+              <li class="list-group-item" v-if="box.category">
+                <b>Category</b>
+                <a class="pull-right">{{ box.category }}</a><br>
+              </li>
+              <li class="list-group-item" v-if="box.releasedate">
+                <b>Release date</b>
+                <a class="pull-right">{{ box.releasedate }}</a><br>
+              </li>
+              <li class="list-group-item" v-if="box.specifications">
+                <b>Specifications</b>
+                <a class="pull-right">{{ box.specifications }}</a><br>
+              </li>
+              <li class="list-group-item" v-if="box.sculptor">
+                <b>Sculptor</b>
+                <a class="pull-right">{{ box.sculptor }}</a><br>
+              </li>
+              <li class="list-group-item" v-if="box.cooperation">
+                <b>Cooperation</b>
+                <a class="pull-right">{{ box.cooperation }}</a><br>
+              </li>
+              <li class="list-group-item" v-if="box.officialurl">
+                <b>Links</b>
+                <a class="pull-right" :href="box.officialurl">GSC product page</a><br>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-12 col-xs-12">
+        <div class="box">
+          <box-header title="Photo" collapsable="true" icon="fa-photo"></box-header>
+          <div class="box-body">
+            <img :src="resources.imagesurl+'/images/nendos/boxes/'+box.internalid+'_thumb'" />
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
 import Resources from './../config/resources'
+import BoxHeader from './layouts/BoxHeader'
 
 export default {
   name: 'Box',
+  components: {
+    BoxHeader
+  },
   data () {
     return {
       resources: Resources,
@@ -39,3 +110,13 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .list-group-item a {
+    width: calc(100% - 8em);
+    text-align: right;
+  }
+  .pull-right+br {
+    clear: both;
+  }
+</style>
---
+++ nendosfront/src/components/layouts/BoxHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="box-header with-border">
+    <h3 class="box-title"><i v-if="icon" class="fa" :class="icon"></i> {{ title }}</h3>
+    <div class="box-tools pull-right" v-if="collapsable">
+      <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BoxHeader',
+  props: ['title', 'collapsable', 'icon'],
+  data () {
+    return {
+    }
+  }
+}
+</script>
