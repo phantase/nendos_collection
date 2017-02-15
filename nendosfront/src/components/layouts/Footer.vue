3820a5bf7d6a4a4e1d06537d40aeb41cb7c87ea3
Phantase <phantase@free.fr>
2017-02-15 22:43:36 +0100

    Add a slogan for the site

--- nendosfront/src/components/layouts/Footer.vue
+++ nendosfront/src/components/layouts/Footer.vue
@@ -3,7 +3,7 @@
   <footer class="main-footer">
     <!-- To the right -->
     <div class="pull-right hidden-xs">
-      Anything you want
+      Manage your Nendoroids collection
     </div>
     <!-- Default to the left -->
     <strong>Copyright &copy; 2017 <a href="#">Nendoroids db</a>.</strong> All rights reserved.
