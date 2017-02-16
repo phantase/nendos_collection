3c3d5db59abc253c5d0c5d299b1fcdab1f1d4588
Phantase <phantase@free.fr>
2017-02-16 21:36:11 +0100

    Add to Nendoroid.vue the visualization of all elements

--- nendosfront/src/components/Nendoroid.vue
+++ nendosfront/src/components/Nendoroid.vue
@@ -56,6 +56,56 @@
         </div>
       </div>
     </div>
+    <div class="row">
+      <div class="col-md-6 col-sm-12 col-xs-12" v-if="faces.length > 0">
+        <div class="box collapsed-box">
+          <app-box-header title="Faces" collapsable="true" collapsed="true" icon="icon-icon_nendo_face"></app-box-header>
+          <div class="box-body">
+            <faces-tiles :faces="faces" tilessize="big"></faces-tiles>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-sm-12 col-xs-12" v-if="hairs.length > 0">
+        <div class="box collapsed-box">
+          <app-box-header title="Hairs" collapsable="true" collapsed="true" icon="icon-icon_nendo_hair"></app-box-header>
+          <div class="box-body">
+            <hairs-tiles :hairs="hairs" tilessize="big"></hairs-tiles>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-sm-12 col-xs-12" v-if="hands.length > 0">
+        <div class="box collapsed-box">
+          <app-box-header title="Hands" collapsable="true" collapsed="true" icon="icon-icon_nendo_hand"></app-box-header>
+          <div class="box-body">
+            <hands-tiles :hands="hands" tilessize="big"></faces-tiles>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-sm-12 col-xs-12" v-if="bodyparts.length > 0">
+        <div class="box collapsed-box">
+          <app-box-header title="Bodyparts" collapsable="true" collapsed="true" icon="icon-icon_nendo_body"></app-box-header>
+          <div class="box-body">
+            <bodyparts-tiles :bodyparts="bodyparts" tilessize="big"></bodyparts-tiles>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-sm-12 col-xs-12" v-if="accessories.length > 0">
+        <div class="box collapsed-box">
+          <app-box-header title="Accessories" collapsable="true" collapsed="true" icon="icon-icon_nendo_accessories"></app-box-header>
+          <div class="box-body">
+            <accessories-tiles :accessories="accessories" tilessize="big"></accessories-tiles>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-12 col-sm-12 col-xs-12" v-if="photos.length > 0">
+        <div class="box collapsed-box">
+          <app-box-header title="Photos" collapsable="true" collapsed="true" icon="fa-photo"></app-box-header>
+          <div class="box-body">
+            <photos-tiles :photos="photos"></photos-tiles>
+          </div>
+        </div>
+      </div>
+    </div>
 
   </div>
 </template>
@@ -63,30 +113,84 @@
 <script>
 import Resources from './../config/resources'
 import AppBoxHeader from './layouts/BoxHeader'
+import FacesTiles from './dblayouts/FacesTiles'
+import HairsTiles from './dblayouts/HairsTiles'
+import HandsTiles from './dblayouts/HandsTiles'
+import BodypartsTiles from './dblayouts/BodypartsTiles'
+import AccessoriesTiles from './dblayouts/AccessoriesTiles'
+import PhotosTiles from './dblayouts/PhotosTiles'
 
 export default {
   name: 'Nendoroid',
   components: {
-    AppBoxHeader
+    AppBoxHeader,
+    FacesTiles,
+    HairsTiles,
+    HandsTiles,
+    BodypartsTiles,
+    AccessoriesTiles,
+    PhotosTiles
   },
   data () {
     return {
       resources: Resources,
       box: [],
-      nendoroid: []
+      nendoroid: [],
+      faces: [],
+      hairs: [],
+      hands: [],
+      bodyparts: [],
+      accessories: [],
+      photos: []
     }
   },
   mounted () {
     this.$nendoroids = this.$resource('nendoroids{/id}')
-    this.$boxes = this.$resource('boxes{/id}')
+    this.$boxes = this.$resource('nendoroids{/id}/boxes')
+    this.$faces = this.$resource('nendoroids{/id}/faces')
+    this.$hairs = this.$resource('nendoroids{/id}/hairs')
+    this.$hands = this.$resource('nendoroids{/id}/hands')
+    this.$bodyparts = this.$resource('nendoroids{/id}/bodyparts')
+    this.$accessories = this.$resource('nendoroids{/id}/accessories')
+    this.$photos = this.$resource('nendoroids{/id}/photos')
 
     this.$nendoroids.query({id: this.$route.params.id}).then((response) => {
       this.nendoroid = response.data
-      this.$boxes.query({id: this.nendoroid.boxid}).then((response) => {
-        this.box = response.data
-      }, (response) => {
-        console.log('Error', response)
-      })
+    }, (response) => {
+      console.log('Error', response)
+    })
+    this.$boxes.query({id: this.$route.params.id}).then((response) => {
+      this.box = response.data[0]
+    }, (response) => {
+      console.log('Error', response)
+    })
+    this.$faces.query({id: this.$route.params.id}).then((response) => {
+      this.faces = response.data
+    }, (response) => {
+      console.log('Error', response)
+    })
+    this.$hairs.query({id: this.$route.params.id}).then((response) => {
+      this.hairs = response.data
+    }, (response) => {
+      console.log('Error', response)
+    })
+    this.$hands.query({id: this.$route.params.id}).then((response) => {
+      this.hands = response.data
+    }, (response) => {
+      console.log('Error', response)
+    })
+    this.$bodyparts.query({id: this.$route.params.id}).then((response) => {
+      this.bodyparts = response.data
+    }, (response) => {
+      console.log('Error', response)
+    })
+    this.$accessories.query({id: this.$route.params.id}).then((response) => {
+      this.accessories = response.data
+    }, (response) => {
+      console.log('Error', response)
+    })
+    this.$photos.query({id: this.$route.params.id}).then((response) => {
+      this.photos = response.data
     }, (response) => {
       console.log('Error', response)
     })
