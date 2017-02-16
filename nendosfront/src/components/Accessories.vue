6102a8aeb73373b38244768fece2032378fc4c55
Phantase <phantase@free.fr>
2017-02-16 19:26:07 +0100

    Creation of components to view Accessories Tiles

--- nendosfront/src/components/Accessories.vue
+++ nendosfront/src/components/Accessories.vue
@@ -1,29 +1,18 @@
 <template>
   <div class="db-accessories">
-
-    <div class="row">
-      <div class="col-md-2 col-sm-4 col-xs-6" v-for="accessory in accessories">
-        <div class="box box-solid">
-          <div class="box-header with-border">
-            <h3 class="box-title">
-              <div class="db-accessory-internalid">{{ accessory.internalid }}</div>
-            </h3>
-          </div>
-          <div class="box-body db-image">
-            <img :src="resources.imagesurl+'/images/nendos/accessories/'+accessory.internalid+'_thumb'" />
-          </div>
-        </div>
-      </div>
-    </div>
-
+    <accessories-tiles :accessories="accessories"></accessories-tiles>
   </div>
 </template>
 
 <script>
 import Resources from './../config/resources'
+import AccessoriesTiles from './dblayouts/AccessoriesTiles'
 
 export default {
   name: 'Accessories',
+  components: {
+    AccessoriesTiles
+  },
   data () {
     return {
       resources: Resources,
---
+++ nendosfront/src/components/dblayouts/AccessoriesTiles.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="row">
+    <div class="col-md-2 col-sm-4 col-xs-6" v-for="accessory in accessories">
+      <div class="box box-solid">
+        <div class="box-header with-border">
+          <h3 class="box-title">
+            <div class="db-accessory-internalid">{{ accessory.internalid }}</div>
+          </h3>
+        </div>
+        <div class="box-body db-image">
+          <img :src="resources.imagesurl+'/images/nendos/accessories/'+accessory.internalid+'_thumb'" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Resources from './../../config/resources'
+
+export default {
+  name: 'AccessoriesTiles',
+  props: ['accessories'],
+  data () {
+    return {
+      resources: Resources
+    }
+  }
+}
+
+</script>
