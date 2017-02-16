89d398ea9a57d81032c2d534ea4abb402af9b006
Phantase <phantase@free.fr>
2017-02-16 19:30:26 +0100

    Creation of components to view Bodyparts Tiles

--- nendosfront/src/components/Bodyparts.vue
+++ nendosfront/src/components/Bodyparts.vue
@@ -1,29 +1,18 @@
 <template>
   <div class="db-bodyparts">
-
-    <div class="row">
-      <div class="col-md-2 col-sm-4 col-xs-6" v-for="bodypart in bodyparts">
-        <div class="box box-solid">
-          <div class="box-header with-border">
-            <h3 class="box-title">
-              <div class="db-bodypart-internalid">{{ bodypart.internalid }}</div>
-            </h3>
-          </div>
-          <div class="box-body db-image">
-            <img :src="resources.imagesurl+'/images/nendos/bodyparts/'+bodypart.internalid+'_thumb'" />
-          </div>
-        </div>
-      </div>
-    </div>
-
+    <bodyparts-tiles :bodyparts="bodyparts"></bodyparts-tiles>
   </div>
 </template>
 
 <script>
 import Resources from './../config/resources'
+import BodypartsTiles from './dblayouts/BodypartsTiles'
 
 export default {
   name: 'Bodyparts',
+  components: {
+    BodypartsTiles
+  },
   data () {
     return {
       resources: Resources,
---
+++ nendosfront/src/components/dblayouts/BodypartsTiles.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="row">
+    <div class="col-md-2 col-sm-4 col-xs-6" v-for="bodypart in bodyparts">
+      <div class="box box-solid">
+        <div class="box-header with-border">
+          <h3 class="box-title">
+            <div class="db-bodypart-internalid">{{ bodypart.internalid }}</div>
+          </h3>
+        </div>
+        <div class="box-body db-image">
+          <img :src="resources.imagesurl+'/images/nendos/bodyparts/'+bodypart.internalid+'_thumb'" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Resources from './../../config/resources'
+
+export default {
+  name: 'BodypartsTiles',
+  props: ['bodyparts'],
+  data () {
+    return {
+      resources: Resources
+    }
+  }
+}
+
+</script>
