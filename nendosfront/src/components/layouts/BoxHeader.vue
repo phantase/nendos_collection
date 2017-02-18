06e1956c20a4fce9ead7c69ae8ca6cfc5a58fc97
Phantase <phantase@free.fr>
2017-02-18 12:39:21 +0100

    Add tooltip to the button used to collapse a box

--- nendosfront/src/components/layouts/BoxHeader.vue
+++ nendosfront/src/components/layouts/BoxHeader.vue
@@ -2,7 +2,7 @@
   <div class="box-header with-border">
     <h3 class="box-title"><i v-if="icon" class="fa" :class="icon"></i> {{ title }}</h3>
     <div class="box-tools pull-right" v-if="collapsable">
-      <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa" :class="collapsed?'fa-plus':'fa-minus'"></i>
+      <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse"><i class="fa" :class="collapsed?'fa-plus':'fa-minus'"></i>
     </div>
   </div>
 </template>
