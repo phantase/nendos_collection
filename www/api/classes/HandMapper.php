575e894684439acdcbf36826e6e42cc2200091b1
Phantase <phantase@free.fr>
2017-02-23 23:10:09 +0100

    Add collection to Hand Mapper in API

--- www/api/classes/HandMapper.php
+++ www/api/classes/HandMapper.php
@@ -4,16 +4,23 @@ class HandMapper extends Mapper
 {
   protected $tablename = "hands";
 
-  public function get() {
+  public function get($userid=null) {
     $sql = "SELECT h.internalid, h.boxid, h.nendoroidid, h.skin_color, h.leftright, h.posture, h.description,
                   h.creatorid, uc.username AS creatorname, h.creationdate,
                   h.editorid, ue.username AS editorname, h.editiondate,
-                  h.validatorid, uv.username AS validatorname, h.validationdate
+                  h.validatorid, uv.username AS validatorname, h.validationdate,
+                  ucol.additiondate AS colladdeddate, ucol.quantity AS collquantity
             FROM hands h
             LEFT JOIN users uc ON h.creatorid = uc.internalid
             LEFT JOIN users ue ON h.editorid = ue.internalid
-            LEFT JOIN users uv ON h.validatorid = uv.internalid";
-    $stmt = $this->db->query($sql);
+            LEFT JOIN users uv ON h.validatorid = uv.internalid
+            LEFT JOIN (
+                  SELECT handid, additiondate, quantity
+                  FROM users_hands_collection
+                  WHERE userid = :userid
+                  ) AS ucol ON h.internalid = ucol.handid";
+    $stmt = $this->db->prepare($sql);
+    $result = $stmt->execute(["userid" => $userid]);
 
     $results = [];
     while ($row = $stmt->fetch()) {
@@ -22,98 +29,128 @@ class HandMapper extends Mapper
     return $results;
   }
 
-  public function getByInternalid($hand_internalid) {
+  public function getByInternalid($hand_internalid, $userid=null) {
     $sql = "SELECT h.internalid, h.boxid, h.nendoroidid, h.skin_color, h.leftright, h.posture, h.description,
                   h.creatorid, uc.username AS creatorname, h.creationdate,
                   h.editorid, ue.username AS editorname, h.editiondate,
-                  h.validatorid, uv.username AS validatorname, h.validationdate
+                  h.validatorid, uv.username AS validatorname, h.validationdate,
+                  ucol.additiondate AS colladdeddate, ucol.quantity AS collquantity
             FROM hands h
             LEFT JOIN users uc ON h.creatorid = uc.internalid
             LEFT JOIN users ue ON h.editorid = ue.internalid
             LEFT JOIN users uv ON h.validatorid = uv.internalid
+            LEFT JOIN (
+                  SELECT handid, additiondate, quantity
+                  FROM users_hands_collection
+                  WHERE userid = :userid
+                  ) AS ucol ON h.internalid = ucol.handid
             WHERE h.internalid = :hand_internalid";
     $stmt = $this->db->prepare($sql);
-    $result = $stmt->execute(["hand_internalid" => $hand_internalid]);
+    $result = $stmt->execute(["hand_internalid" => $hand_internalid, "userid" => $userid]);
 
     if($row = $stmt->fetch()) {
       return new HandEntity($row);
     }
   }
 
-  public function getByBoxid($boxid) {
-    $sql = "SELECT h.internalid, h.boxid, h.nendoroidid, h.skin_color, h.leftright, h.posture, h.description,
-                  h.creatorid, uc.username AS creatorname, h.creationdate,
-                  h.editorid, ue.username AS editorname, h.editiondate,
-                  h.validatorid, uv.username AS validatorname, h.validationdate
-            FROM hands h
-            LEFT JOIN users uc ON h.creatorid = uc.internalid
-            LEFT JOIN users ue ON h.editorid = ue.internalid
-            LEFT JOIN users uv ON h.validatorid = uv.internalid
-            WHERE h.boxid = :boxid";
-    $stmt = $this->db->prepare($sql);
-    $result = $stmt->execute(["boxid" => $boxid]);
-
-    $results = [];
-    while ($row = $stmt->fetch()) {
-      $results[] = new HandEntity($row);
-    }
-    return $results;
-  }
-
-  public function getByNendoroidid($nendoroidid) {
-    $sql = "SELECT h.internalid, h.boxid, h.nendoroidid, h.skin_color, h.leftright, h.posture, h.description,
-                  h.creatorid, uc.username AS creatorname, h.creationdate,
-                  h.editorid, ue.username AS editorname, h.editiondate,
-                  h.validatorid, uv.username AS validatorname, h.validationdate
-            FROM hands h
-            LEFT JOIN users uc ON h.creatorid = uc.internalid
-            LEFT JOIN users ue ON h.editorid = ue.internalid
-            LEFT JOIN users uv ON h.validatorid = uv.internalid
-            WHERE h.nendoroidid = :nendoroidid";
-    $stmt = $this->db->prepare($sql);
-    $result = $stmt->execute(["nendoroidid" => $nendoroidid]);
-
-    $results = [];
-    while ($row = $stmt->fetch()) {
-      $results[] = new HandEntity($row);
-    }
-    return $results;
-  }
-
-  public function getByHandid($handid) {
-    $sql = "SELECT h.internalid, h.boxid, h.nendoroidid, h.skin_color, h.leftright, h.posture, h.description,
-                  h.creatorid, uc.username AS creatorname, h.creationdate,
-                  h.editorid, ue.username AS editorname, h.editiondate,
-                  h.validatorid, uv.username AS validatorname, h.validationdate
-            FROM hands h
-            LEFT JOIN users uc ON h.creatorid = uc.internalid
-            LEFT JOIN users ue ON h.editorid = ue.internalid
-            LEFT JOIN users uv ON h.validatorid = uv.internalid
-            WHERE h.internalid = :handid";
-    $stmt = $this->db->prepare($sql);
-    $result = $stmt->execute(["handid" => $handid]);
-
-    $results = [];
-    while ($row = $stmt->fetch()) {
-      $results[] = new HandEntity($row);
-    }
-    return $results;
-  }
-
-  public function getByPhotoid($photoid) {
+  public function getByBoxid($boxid, $userid=null) {
     $sql = "SELECT h.internalid, h.boxid, h.nendoroidid, h.skin_color, h.leftright, h.posture, h.description,
                   h.creatorid, uc.username AS creatorname, h.creationdate,
                   h.editorid, ue.username AS editorname, h.editiondate,
                   h.validatorid, uv.username AS validatorname, h.validationdate,
-                  ph.xmin, ph.xmax, ph.ymin, ph.ymax, ph.internalid AS photoannotationid
+                  ucol.additiondate AS colladdeddate, ucol.quantity AS collquantity
+            FROM hands h
+            LEFT JOIN users uc ON h.creatorid = uc.internalid
+            LEFT JOIN users ue ON h.editorid = ue.internalid
+            LEFT JOIN users uv ON h.validatorid = uv.internalid
+            LEFT JOIN (
+                  SELECT handid, additiondate, quantity
+                  FROM users_hands_collection
+                  WHERE userid = :userid
+                  ) AS ucol ON h.internalid = ucol.handid
+            WHERE h.boxid = :boxid";
+    $stmt = $this->db->prepare($sql);
+    $result = $stmt->execute(["boxid" => $boxid, "userid" => $userid]);
+
+    $results = [];
+    while ($row = $stmt->fetch()) {
+      $results[] = new HandEntity($row);
+    }
+    return $results;
+  }
+
+  public function getByNendoroidid($nendoroidid, $userid=null) {
+    $sql = "SELECT h.internalid, h.boxid, h.nendoroidid, h.skin_color, h.leftright, h.posture, h.description,
+                  h.creatorid, uc.username AS creatorname, h.creationdate,
+                  h.editorid, ue.username AS editorname, h.editiondate,
+                  h.validatorid, uv.username AS validatorname, h.validationdate,
+                  ucol.additiondate AS colladdeddate, ucol.quantity AS collquantity
+            FROM hands h
+            LEFT JOIN users uc ON h.creatorid = uc.internalid
+            LEFT JOIN users ue ON h.editorid = ue.internalid
+            LEFT JOIN users uv ON h.validatorid = uv.internalid
+            LEFT JOIN (
+                  SELECT handid, additiondate, quantity
+                  FROM users_hands_collection
+                  WHERE userid = :userid
+                  ) AS ucol ON h.internalid = ucol.handid
+            WHERE h.nendoroidid = :nendoroidid";
+    $stmt = $this->db->prepare($sql);
+    $result = $stmt->execute(["nendoroidid" => $nendoroidid, "userid" => $userid]);
+
+    $results = [];
+    while ($row = $stmt->fetch()) {
+      $results[] = new HandEntity($row);
+    }
+    return $results;
+  }
+
+  public function getByHandid($handid, $userid=null) {
+    $sql = "SELECT h.internalid, h.boxid, h.nendoroidid, h.skin_color, h.leftright, h.posture, h.description,
+                  h.creatorid, uc.username AS creatorname, h.creationdate,
+                  h.editorid, ue.username AS editorname, h.editiondate,
+                  h.validatorid, uv.username AS validatorname, h.validationdate,
+                  ucol.additiondate AS colladdeddate, ucol.quantity AS collquantity
+            FROM hands h
+            LEFT JOIN users uc ON h.creatorid = uc.internalid
+            LEFT JOIN users ue ON h.editorid = ue.internalid
+            LEFT JOIN users uv ON h.validatorid = uv.internalid
+            LEFT JOIN (
+                  SELECT handid, additiondate, quantity
+                  FROM users_hands_collection
+                  WHERE userid = :userid
+                  ) AS ucol ON h.internalid = ucol.handid
+            WHERE h.internalid = :handid";
+    $stmt = $this->db->prepare($sql);
+    $result = $stmt->execute(["handid" => $handid, "userid" => $userid]);
+
+    $results = [];
+    while ($row = $stmt->fetch()) {
+      $results[] = new HandEntity($row);
+    }
+    return $results;
+  }
+
+  public function getByPhotoid($photoid, $userid=null) {
+    $sql = "SELECT h.internalid, h.boxid, h.nendoroidid, h.skin_color, h.leftright, h.posture, h.description,
+                  h.creatorid, uc.username AS creatorname, h.creationdate,
+                  h.editorid, ue.username AS editorname, h.editiondate,
+                  h.validatorid, uv.username AS validatorname, h.validationdate,
+                  ph.xmin, ph.xmax, ph.ymin, ph.ymax, ph.internalid AS photoannotationid,
+                  ucol.additiondate AS colladdeddate, ucol.quantity AS collquantity
             FROM hands h
             LEFT JOIN users uc ON h.creatorid = uc.internalid
             LEFT JOIN users ue ON h.editorid = ue.internalid
             LEFT JOIN users uv ON h.validatorid = uv.internalid
             LEFT JOIN photos_hands ph ON h.internalid = ph.handid
+            LEFT JOIN (
+                  SELECT handid, additiondate, quantity
+                  FROM users_hands_collection
+                  WHERE userid = :userid
+                  ) AS ucol ON h.internalid = ucol.handid
             WHERE ph.photoid = :photoid";
     $stmt = $this->db->prepare($sql);
-    $result = $stmt->execute(["photoid" => $photoid]);
+    $result = $stmt->execute(["photoid" => $photoid, "userid" => $userid]);
 
     $results = [];
     while ($row = $stmt->fetch()) {
