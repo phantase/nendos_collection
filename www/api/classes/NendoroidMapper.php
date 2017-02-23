affdeec5c294bbfe9e243246e51f4192fabc1f30
Phantase <phantase@free.fr>
2017-02-23 22:55:43 +0100

    Add collection to Nendoroid Mapper in API

--- www/api/classes/NendoroidMapper.php
+++ www/api/classes/NendoroidMapper.php
@@ -4,16 +4,23 @@ class NendoroidMapper extends Mapper
 {
   protected $tablename = "nendoroids";
 
-  public function get() {
+  public function get($userid=null) {
     $sql = "SELECT n.internalid, n.boxid, n.name, n.version, n.sex, n.dominant_color,
                   n.creatorid, uc.username AS creatorname, n.creationdate,
                   n.editorid, ue.username AS editorname, n.editiondate,
-                  n.validatorid, uv.username AS validatorname, n.validationdate
+                  n.validatorid, uv.username AS validatorname, n.validationdate,
+                  ucol.additiondate AS colladdeddate, ucol.quantity AS collquantity
             FROM nendoroids n
             LEFT JOIN users uc ON n.creatorid = uc.internalid
             LEFT JOIN users ue ON n.editorid = ue.internalid
-            LEFT JOIN users uv ON n.validatorid = uv.internalid";
-    $stmt = $this->db->query($sql);
+            LEFT JOIN users uv ON n.validatorid = uv.internalid
+            LEFT JOIN (
+                  SELECT nendoroidid, additiondate, quantity
+                  FROM users_nendoroids_collection
+                  WHERE userid = :userid
+                  ) AS ucol ON n.internalid = ucol.nendoroidid";
+    $stmt = $this->db->prepare($sql);
+    $result = $stmt->execute(["userid" => $userid]);
 
     $results = [];
     while ($row = $stmt->fetch()) {
@@ -22,36 +29,48 @@ class NendoroidMapper extends Mapper
     return $results;
   }
 
-  public function getByInternalid($nendoroid_internalid) {
+  public function getByInternalid($nendoroid_internalid, $userid=null) {
     $sql = "SELECT n.internalid, n.boxid, n.name, n.version, n.sex, n.dominant_color,
                   n.creatorid, uc.username AS creatorname, n.creationdate,
                   n.editorid, ue.username AS editorname, n.editiondate,
-                  n.validatorid, uv.username AS validatorname, n.validationdate
+                  n.validatorid, uv.username AS validatorname, n.validationdate,
+                  ucol.additiondate AS colladdeddate, ucol.quantity AS collquantity
             FROM nendoroids n
             LEFT JOIN users uc ON n.creatorid = uc.internalid
             LEFT JOIN users ue ON n.editorid = ue.internalid
             LEFT JOIN users uv ON n.validatorid = uv.internalid
+            LEFT JOIN (
+                  SELECT nendoroidid, additiondate, quantity
+                  FROM users_nendoroids_collection
+                  WHERE userid = :userid
+                  ) AS ucol ON n.internalid = ucol.nendoroidid
             WHERE n.internalid = :nendoroid_internalid";
     $stmt = $this->db->prepare($sql);
-    $result = $stmt->execute(["nendoroid_internalid" => $nendoroid_internalid]);
+    $result = $stmt->execute(["nendoroid_internalid" => $nendoroid_internalid, "userid" => $userid]);
 
     if($row = $stmt->fetch()) {
       return new NendoroidEntity($row);
     }
   }
 
-  public function getByBoxid($boxid) {
+  public function getByBoxid($boxid, $userid=null) {
     $sql = "SELECT n.internalid, n.boxid, n.name, n.version, n.sex, n.dominant_color,
                   n.creatorid, uc.username AS creatorname, n.creationdate,
                   n.editorid, ue.username AS editorname, n.editiondate,
-                  n.validatorid, uv.username AS validatorname, n.validationdate
+                  n.validatorid, uv.username AS validatorname, n.validationdate,
+                  ucol.additiondate AS colladdeddate, ucol.quantity AS collquantity
             FROM nendoroids n
             LEFT JOIN users uc ON n.creatorid = uc.internalid
             LEFT JOIN users ue ON n.editorid = ue.internalid
             LEFT JOIN users uv ON n.validatorid = uv.internalid
+            LEFT JOIN (
+                  SELECT nendoroidid, additiondate, quantity
+                  FROM users_nendoroids_collection
+                  WHERE userid = :userid
+                  ) AS ucol ON n.internalid = ucol.nendoroidid
             WHERE n.boxid = :boxid";
     $stmt = $this->db->prepare($sql);
-    $result = $stmt->execute(["boxid" => $boxid]);
+    $result = $stmt->execute(["boxid" => $boxid, "userid" => $userid]);
 
     $results = [];
     while ($row = $stmt->fetch()) {
@@ -60,18 +79,24 @@ class NendoroidMapper extends Mapper
     return $results;
   }
 
-  public function getByNendoroidid($nendoroidid) {
+  public function getByNendoroidid($nendoroidid, $userid=null) {
     $sql = "SELECT n.internalid, n.boxid, n.name, n.version, n.sex, n.dominant_color,
                   n.creatorid, uc.username AS creatorname, n.creationdate,
                   n.editorid, ue.username AS editorname, n.editiondate,
-                  n.validatorid, uv.username AS validatorname, n.validationdate
+                  n.validatorid, uv.username AS validatorname, n.validationdate,
+                  ucol.additiondate AS colladdeddate, ucol.quantity AS collquantity
             FROM nendoroids n
             LEFT JOIN users uc ON n.creatorid = uc.internalid
             LEFT JOIN users ue ON n.editorid = ue.internalid
             LEFT JOIN users uv ON n.validatorid = uv.internalid
+            LEFT JOIN (
+                  SELECT nendoroidid, additiondate, quantity
+                  FROM users_nendoroids_collection
+                  WHERE userid = :userid
+                  ) AS ucol ON n.internalid = ucol.nendoroidid
             WHERE n.internalid = :nendoroidid";
     $stmt = $this->db->prepare($sql);
-    $result = $stmt->execute(["nendoroidid" => $nendoroidid]);
+    $result = $stmt->execute(["nendoroidid" => $nendoroidid, "userid" => $userid]);
 
     $results = [];
     while ($row = $stmt->fetch()) {
@@ -80,19 +105,25 @@ class NendoroidMapper extends Mapper
     return $results;
   }
 
-  public function getByAccessoryid($accessoryid) {
+  public function getByAccessoryid($accessoryid, $userid=null) {
     $sql = "SELECT n.internalid, n.boxid, n.name, n.version, n.sex, n.dominant_color,
                   n.creatorid, uc.username AS creatorname, n.creationdate,
                   n.editorid, ue.username AS editorname, n.editiondate,
-                  n.validatorid, uv.username AS validatorname, n.validationdate
+                  n.validatorid, uv.username AS validatorname, n.validationdate,
+                  ucol.additiondate AS colladdeddate, ucol.quantity AS collquantity
             FROM nendoroids n
             LEFT JOIN users uc ON n.creatorid = uc.internalid
             LEFT JOIN users ue ON n.editorid = ue.internalid
             LEFT JOIN users uv ON n.validatorid = uv.internalid
             LEFT JOIN accessories a ON n.internalid = a.nendoroidid
+            LEFT JOIN (
+                  SELECT nendoroidid, additiondate, quantity
+                  FROM users_nendoroids_collection
+                  WHERE userid = :userid
+                  ) AS ucol ON n.internalid = ucol.nendoroidid
             WHERE a.internalid = :accessoryid";
     $stmt = $this->db->prepare($sql);
-    $result = $stmt->execute(["accessoryid" => $accessoryid]);
+    $result = $stmt->execute(["accessoryid" => $accessoryid, "userid" => $userid]);
 
     $results = [];
     while ($row = $stmt->fetch()) {
@@ -101,19 +132,25 @@ class NendoroidMapper extends Mapper
     return $results;
   }
 
-  public function getByBodypartid($bodypartid) {
+  public function getByBodypartid($bodypartid, $userid=null) {
     $sql = "SELECT n.internalid, n.boxid, n.name, n.version, n.sex, n.dominant_color,
                   n.creatorid, uc.username AS creatorname, n.creationdate,
                   n.editorid, ue.username AS editorname, n.editiondate,
-                  n.validatorid, uv.username AS validatorname, n.validationdate
+                  n.validatorid, uv.username AS validatorname, n.validationdate,
+                  ucol.additiondate AS colladdeddate, ucol.quantity AS collquantity
             FROM nendoroids n
             LEFT JOIN users uc ON n.creatorid = uc.internalid
             LEFT JOIN users ue ON n.editorid = ue.internalid
             LEFT JOIN users uv ON n.validatorid = uv.internalid
             LEFT JOIN bodyparts bp ON n.internalid = bp.nendoroidid
+            LEFT JOIN (
+                  SELECT nendoroidid, additiondate, quantity
+                  FROM users_nendoroids_collection
+                  WHERE userid = :userid
+                  ) AS ucol ON n.internalid = ucol.nendoroidid
             WHERE bp.internalid = :bodypartid";
     $stmt = $this->db->prepare($sql);
-    $result = $stmt->execute(["bodypartid" => $bodypartid]);
+    $result = $stmt->execute(["bodypartid" => $bodypartid, "userid" => $userid]);
 
     $results = [];
     while ($row = $stmt->fetch()) {
@@ -122,19 +159,25 @@ class NendoroidMapper extends Mapper
     return $results;
   }
 
-  public function getByFaceid($faceid) {
+  public function getByFaceid($faceid, $userid=null) {
     $sql = "SELECT n.internalid, n.boxid, n.name, n.version, n.sex, n.dominant_color,
                   n.creatorid, uc.username AS creatorname, n.creationdate,
                   n.editorid, ue.username AS editorname, n.editiondate,
-                  n.validatorid, uv.username AS validatorname, n.validationdate
+                  n.validatorid, uv.username AS validatorname, n.validationdate,
+                  ucol.additiondate AS colladdeddate, ucol.quantity AS collquantity
             FROM nendoroids n
             LEFT JOIN users uc ON n.creatorid = uc.internalid
             LEFT JOIN users ue ON n.editorid = ue.internalid
             LEFT JOIN users uv ON n.validatorid = uv.internalid
             LEFT JOIN faces f ON n.internalid = f.nendoroidid
+            LEFT JOIN (
+                  SELECT nendoroidid, additiondate, quantity
+                  FROM users_nendoroids_collection
+                  WHERE userid = :userid
+                  ) AS ucol ON n.internalid = ucol.nendoroidid
             WHERE f.internalid = :faceid";
     $stmt = $this->db->prepare($sql);
-    $result = $stmt->execute(["faceid" => $faceid]);
+    $result = $stmt->execute(["faceid" => $faceid, "userid" => $userid]);
 
     $results = [];
     while ($row = $stmt->fetch()) {
@@ -143,19 +186,25 @@ class NendoroidMapper extends Mapper
     return $results;
   }
 
-  public function getByHairid($hairid) {
+  public function getByHairid($hairid, $userid=null) {
     $sql = "SELECT n.internalid, n.boxid, n.name, n.version, n.sex, n.dominant_color,
                   n.creatorid, uc.username AS creatorname, n.creationdate,
                   n.editorid, ue.username AS editorname, n.editiondate,
-                  n.validatorid, uv.username AS validatorname, n.validationdate
+                  n.validatorid, uv.username AS validatorname, n.validationdate,
+                  ucol.additiondate AS colladdeddate, ucol.quantity AS collquantity
             FROM nendoroids n
             LEFT JOIN users uc ON n.creatorid = uc.internalid
             LEFT JOIN users ue ON n.editorid = ue.internalid
             LEFT JOIN users uv ON n.validatorid = uv.internalid
             LEFT JOIN hairs h ON n.internalid = h.nendoroidid
+            LEFT JOIN (
+                  SELECT nendoroidid, additiondate, quantity
+                  FROM users_nendoroids_collection
+                  WHERE userid = :userid
+                  ) AS ucol ON n.internalid = ucol.nendoroidid
             WHERE h.internalid = :hairid";
     $stmt = $this->db->prepare($sql);
-    $result = $stmt->execute(["hairid" => $hairid]);
+    $result = $stmt->execute(["hairid" => $hairid, "userid" => $userid]);
 
     $results = [];
     while ($row = $stmt->fetch()) {
@@ -164,19 +213,25 @@ class NendoroidMapper extends Mapper
     return $results;
   }
 
-  public function getByHandid($handid) {
+  public function getByHandid($handid, $userid=null) {
     $sql = "SELECT n.internalid, n.boxid, n.name, n.version, n.sex, n.dominant_color,
                   n.creatorid, uc.username AS creatorname, n.creationdate,
                   n.editorid, ue.username AS editorname, n.editiondate,
-                  n.validatorid, uv.username AS validatorname, n.validationdate
+                  n.validatorid, uv.username AS validatorname, n.validationdate,
+                  ucol.additiondate AS colladdeddate, ucol.quantity AS collquantity
             FROM nendoroids n
             LEFT JOIN users uc ON n.creatorid = uc.internalid
             LEFT JOIN users ue ON n.editorid = ue.internalid
             LEFT JOIN users uv ON n.validatorid = uv.internalid
             LEFT JOIN hands h ON n.internalid = h.nendoroidid
+            LEFT JOIN (
+                  SELECT nendoroidid, additiondate, quantity
+                  FROM users_nendoroids_collection
+                  WHERE userid = :userid
+                  ) AS ucol ON n.internalid = ucol.nendoroidid
             WHERE h.internalid = :handid";
     $stmt = $this->db->prepare($sql);
-    $result = $stmt->execute(["handid" => $handid]);
+    $result = $stmt->execute(["handid" => $handid, "userid" => $userid]);
 
     $results = [];
     while ($row = $stmt->fetch()) {
@@ -185,20 +240,26 @@ class NendoroidMapper extends Mapper
     return $results;
   }
 
-  public function getByPhotoid($photoid) {
+  public function getByPhotoid($photoid, $userid=null) {
     $sql = "SELECT n.internalid, n.boxid, n.name, n.version, n.sex, n.dominant_color,
                   n.creatorid, uc.username AS creatorname, n.creationdate,
                   n.editorid, ue.username AS editorname, n.editiondate,
                   n.validatorid, uv.username AS validatorname, n.validationdate,
-                  pn.xmin, pn.xmax, pn.ymin, pn.ymax, pn.internalid AS photoannotationid
+                  pn.xmin, pn.xmax, pn.ymin, pn.ymax, pn.internalid AS photoannotationid,
+                  ucol.additiondate AS colladdeddate, ucol.quantity AS collquantity
             FROM nendoroids n
             LEFT JOIN users uc ON n.creatorid = uc.internalid
             LEFT JOIN users ue ON n.editorid = ue.internalid
             LEFT JOIN users uv ON n.validatorid = uv.internalid
             LEFT JOIN photos_nendoroids pn ON n.internalid = pn.nendoroidid
+            LEFT JOIN (
+                  SELECT nendoroidid, additiondate, quantity
+                  FROM users_nendoroids_collection
+                  WHERE userid = :userid
+                  ) AS ucol ON n.internalid = ucol.nendoroidid
             WHERE pn.photoid = :photoid";
     $stmt = $this->db->prepare($sql);
-    $result = $stmt->execute(["photoid" => $photoid]);
+    $result = $stmt->execute(["photoid" => $photoid, "userid" => $userid]);
 
     $results = [];
     while ($row = $stmt->fetch()) {
