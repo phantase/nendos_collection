a96616a508661394df8f1c3cdf921eb4e02f1abe
Phantase <phantase@free.fr>
2017-02-23 23:13:38 +0100

    Add collection to Accessory Mapper in API

--- www/api/classes/AccessoryMapper.php
+++ www/api/classes/AccessoryMapper.php
@@ -4,16 +4,23 @@ class AccessoryMapper extends Mapper
 {
   protected $tablename = "accessories";
 
-  public function get() {
+  public function get($userid=null) {
     $sql = "SELECT a.internalid, a.boxid, a.nendoroidid, a.type, a.main_color, a.other_color, a.description,
                   a.creatorid, uc.username AS creatorname, a.creationdate,
                   a.editorid, ue.username AS editorname, a.editiondate,
-                  a.validatorid, uv.username AS validatorname, a.validationdate
+                  a.validatorid, uv.username AS validatorname, a.validationdate,
+                  ucol.additiondate AS colladdeddate, ucol.quantity AS collquantity
             FROM accessories a
             LEFT JOIN users uc ON a.creatorid = uc.internalid
             LEFT JOIN users ue ON a.editorid = ue.internalid
-            LEFT JOIN users uv ON a.validatorid = uv.internalid";
-    $stmt = $this->db->query($sql);
+            LEFT JOIN users uv ON a.validatorid = uv.internalid
+            LEFT JOIN (
+                  SELECT accessoryid, additiondate, quantity
+                  FROM users_accessories_collection
+                  WHERE userid = :userid
+                  ) AS ucol ON a.internalid = ucol.accessoryid";
+    $stmt = $this->db->prepare($sql);
+    $result = $stmt->execute(["userid" => $userid]);
 
     $results = [];
     while ($row = $stmt->fetch()) {
@@ -22,98 +29,128 @@ class AccessoryMapper extends Mapper
     return $results;
   }
 
-  public function getByInternalid($accessory_internalid) {
+  public function getByInternalid($accessory_internalid, $userid=null) {
     $sql = "SELECT a.internalid, a.boxid, a.nendoroidid, a.type, a.main_color, a.other_color, a.description,
                   a.creatorid, uc.username AS creatorname, a.creationdate,
                   a.editorid, ue.username AS editorname, a.editiondate,
-                  a.validatorid, uv.username AS validatorname, a.validationdate
+                  a.validatorid, uv.username AS validatorname, a.validationdate,
+                  ucol.additiondate AS colladdeddate, ucol.quantity AS collquantity
             FROM accessories a
             LEFT JOIN users uc ON a.creatorid = uc.internalid
             LEFT JOIN users ue ON a.editorid = ue.internalid
             LEFT JOIN users uv ON a.validatorid = uv.internalid
+            LEFT JOIN (
+                  SELECT accessoryid, additiondate, quantity
+                  FROM users_accessories_collection
+                  WHERE userid = :userid
+                  ) AS ucol ON a.internalid = ucol.accessoryid
             WHERE a.internalid = :accessory_internalid";
     $stmt = $this->db->prepare($sql);
-    $result = $stmt->execute(["accessory_internalid" => $accessory_internalid]);
+    $result = $stmt->execute(["accessory_internalid" => $accessory_internalid, "userid" => $userid]);
 
     if($row = $stmt->fetch()) {
       return new AccessoryEntity($row);
     }
   }
 
-  public function getByBoxid($boxid) {
-    $sql = "SELECT a.internalid, a.boxid, a.nendoroidid, a.type, a.main_color, a.other_color, a.description,
-                  a.creatorid, uc.username AS creatorname, a.creationdate,
-                  a.editorid, ue.username AS editorname, a.editiondate,
-                  a.validatorid, uv.username AS validatorname, a.validationdate
-            FROM accessories a
-            LEFT JOIN users uc ON a.creatorid = uc.internalid
-            LEFT JOIN users ue ON a.editorid = ue.internalid
-            LEFT JOIN users uv ON a.validatorid = uv.internalid
-            WHERE a.boxid = :boxid";
-    $stmt = $this->db->prepare($sql);
-    $result = $stmt->execute(["boxid" => $boxid]);
-
-    $results = [];
-    while ($row = $stmt->fetch()) {
-      $results[] = new AccessoryEntity($row);
-    }
-    return $results;
-  }
-
-  public function getByNendoroidid($nendoroidid) {
-    $sql = "SELECT a.internalid, a.boxid, a.nendoroidid, a.type, a.main_color, a.other_color, a.description,
-                  a.creatorid, uc.username AS creatorname, a.creationdate,
-                  a.editorid, ue.username AS editorname, a.editiondate,
-                  a.validatorid, uv.username AS validatorname, a.validationdate
-            FROM accessories a
-            LEFT JOIN users uc ON a.creatorid = uc.internalid
-            LEFT JOIN users ue ON a.editorid = ue.internalid
-            LEFT JOIN users uv ON a.validatorid = uv.internalid
-            WHERE a.nendoroidid = :nendoroidid";
-    $stmt = $this->db->prepare($sql);
-    $result = $stmt->execute(["nendoroidid" => $nendoroidid]);
-
-    $results = [];
-    while ($row = $stmt->fetch()) {
-      $results[] = new AccessoryEntity($row);
-    }
-    return $results;
-  }
-
-  public function getByAccessoryid($accessoryid) {
-    $sql = "SELECT a.internalid, a.boxid, a.nendoroidid, a.type, a.main_color, a.other_color, a.description,
-                  a.creatorid, uc.username AS creatorname, a.creationdate,
-                  a.editorid, ue.username AS editorname, a.editiondate,
-                  a.validatorid, uv.username AS validatorname, a.validationdate
-            FROM accessories a
-            LEFT JOIN users uc ON a.creatorid = uc.internalid
-            LEFT JOIN users ue ON a.editorid = ue.internalid
-            LEFT JOIN users uv ON a.validatorid = uv.internalid
-            WHERE a.internalid = :accessoryid";
-    $stmt = $this->db->prepare($sql);
-    $result = $stmt->execute(["accessoryid" => $accessoryid]);
-
-    $results = [];
-    while ($row = $stmt->fetch()) {
-      $results[] = new AccessoryEntity($row);
-    }
-    return $results;
-  }
-
-  public function getByPhotoid($photoid) {
+  public function getByBoxid($boxid, $userid=null) {
     $sql = "SELECT a.internalid, a.boxid, a.nendoroidid, a.type, a.main_color, a.other_color, a.description,
                   a.creatorid, uc.username AS creatorname, a.creationdate,
                   a.editorid, ue.username AS editorname, a.editiondate,
                   a.validatorid, uv.username AS validatorname, a.validationdate,
-                  pa.xmin, pa.xmax, pa.ymin, pa.ymax, pa.internalid AS photoannotationid
+                  ucol.additiondate AS colladdeddate, ucol.quantity AS collquantity
+            FROM accessories a
+            LEFT JOIN users uc ON a.creatorid = uc.internalid
+            LEFT JOIN users ue ON a.editorid = ue.internalid
+            LEFT JOIN users uv ON a.validatorid = uv.internalid
+            LEFT JOIN (
+                  SELECT accessoryid, additiondate, quantity
+                  FROM users_accessories_collection
+                  WHERE userid = :userid
+                  ) AS ucol ON a.internalid = ucol.accessoryid
+            WHERE a.boxid = :boxid";
+    $stmt = $this->db->prepare($sql);
+    $result = $stmt->execute(["boxid" => $boxid, "userid" => $userid]);
+
+    $results = [];
+    while ($row = $stmt->fetch()) {
+      $results[] = new AccessoryEntity($row);
+    }
+    return $results;
+  }
+
+  public function getByNendoroidid($nendoroidid, $userid=null) {
+    $sql = "SELECT a.internalid, a.boxid, a.nendoroidid, a.type, a.main_color, a.other_color, a.description,
+                  a.creatorid, uc.username AS creatorname, a.creationdate,
+                  a.editorid, ue.username AS editorname, a.editiondate,
+                  a.validatorid, uv.username AS validatorname, a.validationdate,
+                  ucol.additiondate AS colladdeddate, ucol.quantity AS collquantity
+            FROM accessories a
+            LEFT JOIN users uc ON a.creatorid = uc.internalid
+            LEFT JOIN users ue ON a.editorid = ue.internalid
+            LEFT JOIN users uv ON a.validatorid = uv.internalid
+            LEFT JOIN (
+                  SELECT accessoryid, additiondate, quantity
+                  FROM users_accessories_collection
+                  WHERE userid = :userid
+                  ) AS ucol ON a.internalid = ucol.accessoryid
+            WHERE a.nendoroidid = :nendoroidid";
+    $stmt = $this->db->prepare($sql);
+    $result = $stmt->execute(["nendoroidid" => $nendoroidid, "userid" => $userid]);
+
+    $results = [];
+    while ($row = $stmt->fetch()) {
+      $results[] = new AccessoryEntity($row);
+    }
+    return $results;
+  }
+
+  public function getByAccessoryid($accessoryid, $userid=null) {
+    $sql = "SELECT a.internalid, a.boxid, a.nendoroidid, a.type, a.main_color, a.other_color, a.description,
+                  a.creatorid, uc.username AS creatorname, a.creationdate,
+                  a.editorid, ue.username AS editorname, a.editiondate,
+                  a.validatorid, uv.username AS validatorname, a.validationdate,
+                  ucol.additiondate AS colladdeddate, ucol.quantity AS collquantity
+            FROM accessories a
+            LEFT JOIN users uc ON a.creatorid = uc.internalid
+            LEFT JOIN users ue ON a.editorid = ue.internalid
+            LEFT JOIN users uv ON a.validatorid = uv.internalid
+            LEFT JOIN (
+                  SELECT accessoryid, additiondate, quantity
+                  FROM users_accessories_collection
+                  WHERE userid = :userid
+                  ) AS ucol ON a.internalid = ucol.accessoryid
+            WHERE a.internalid = :accessoryid";
+    $stmt = $this->db->prepare($sql);
+    $result = $stmt->execute(["accessoryid" => $accessoryid, "userid" => $userid]);
+
+    $results = [];
+    while ($row = $stmt->fetch()) {
+      $results[] = new AccessoryEntity($row);
+    }
+    return $results;
+  }
+
+  public function getByPhotoid($photoid, $userid=null) {
+    $sql = "SELECT a.internalid, a.boxid, a.nendoroidid, a.type, a.main_color, a.other_color, a.description,
+                  a.creatorid, uc.username AS creatorname, a.creationdate,
+                  a.editorid, ue.username AS editorname, a.editiondate,
+                  a.validatorid, uv.username AS validatorname, a.validationdate,
+                  pa.xmin, pa.xmax, pa.ymin, pa.ymax, pa.internalid AS photoannotationid,
+                  ucol.additiondate AS colladdeddate, ucol.quantity AS collquantity
             FROM accessories a
             LEFT JOIN users uc ON a.creatorid = uc.internalid
             LEFT JOIN users ue ON a.editorid = ue.internalid
             LEFT JOIN users uv ON a.validatorid = uv.internalid
             LEFT JOIN photos_accessories pa ON a.internalid = pa.accessoryid
+            LEFT JOIN (
+                  SELECT accessoryid, additiondate, quantity
+                  FROM users_accessories_collection
+                  WHERE userid = :userid
+                  ) AS ucol ON a.internalid = ucol.accessoryid
             WHERE pa.photoid = :photoid";
     $stmt = $this->db->prepare($sql);
-    $result = $stmt->execute(["photoid" => $photoid]);
+    $result = $stmt->execute(["photoid" => $photoid, "userid" => $userid]);
 
     $results = [];
     while ($row = $stmt->fetch()) {
