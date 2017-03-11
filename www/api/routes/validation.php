bf760630eb88401074d98fa17670535def346cc3
Phantase <phantase@free.fr>
2017-03-11 10:49:03 +0100

    Fixes #30, validation require validator or administrator rights

--- www/api/routes/validation.php
+++ www/api/routes/validation.php
@@ -3,40 +3,50 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-// Collect an element for a specific user
+// Validate an element for a specific user
 $app->patch('/auth/{element:box|boxes|nendoroid|nendoroids|accessory|accessories|bodypart|bodyparts|face|faces|hair|hairs|hand|hands}/{internalid:[0-9]+}/validate', function(Request $request, Response $response, $args) {
     $userid = $request->getAttribute("token")->user->internalid;
-    $element = $args['element'];
-    $internalid = (int)$args['internalid'];
-    $this->applogger->addInfo("User $userid validates $element $internalid");
-    try {
-        $mapper = MapperFactory::getMapper($this->db,$element);
-        $quantity = $mapper->validateByUser($internalid, $userid);
+    if( $request->getAttribute("token")->user->validator === "1" || $request->getAttribute("token")->user->administrator === "1" ){
+        $element = $args['element'];
+        $internalid = (int)$args['internalid'];
+        $this->applogger->addInfo("User $userid validates $element $internalid");
+        try {
+            $mapper = MapperFactory::getMapper($this->db,$element);
+            $quantity = $mapper->validateByUser($internalid, $userid);
 
-        $newresponse = $response->withJson($quantity);
+            $newresponse = $response->withJson($quantity);
 
-    } catch (Exception $e){
-        $this->applogger->addInfo($e);
-        $newresponse = $response->withJson(null,400);
+        } catch (Exception $e){
+            $this->applogger->addInfo($e);
+            $newresponse = $response->withJson(null,400);
+        }
+    } else {
+        $this->applogger->addInfo("User $userid tries to validate something without the right to do it");
+        $newresponse = $response->withJson(null,403);
     }
     return $newresponse;
 });
 
-// UNCollect an element for a specific user
+// UNValidate an element for a specific user
 $app->patch('/auth/{element:box|boxes|nendoroid|nendoroids|accessory|accessories|bodypart|bodyparts|face|faces|hair|hairs|hand|hands}/{internalid:[0-9]+}/unvalidate', function(Request $request, Response $response, $args) {
     $userid = $request->getAttribute("token")->user->internalid;
-    $element = $args['element'];
-    $internalid = (int)$args['internalid'];
-    $this->applogger->addInfo("User $userid unvalidates $element $internalid");
-    try {
-        $mapper = MapperFactory::getMapper($this->db,$element);
-        $quantity = $mapper->unvalidateByUser($internalid, $userid);
+    if( $request->getAttribute("token")->user->validator === "1" || $request->getAttribute("token")->user->administrator === "1" ){
+        $element = $args['element'];
+        $internalid = (int)$args['internalid'];
+        $this->applogger->addInfo("User $userid unvalidates $element $internalid");
+        try {
+            $mapper = MapperFactory::getMapper($this->db,$element);
+            $quantity = $mapper->unvalidateByUser($internalid, $userid);
 
-        $newresponse = $response->withJson($quantity);
+            $newresponse = $response->withJson($quantity);
 
-    } catch (Exception $e){
-        $this->applogger->addInfo($e);
-        $newresponse = $response->withJson(null,400);
+        } catch (Exception $e){
+            $this->applogger->addInfo($e);
+            $newresponse = $response->withJson(null,400);
+        }
+    } else {
+        $this->applogger->addInfo("User $userid tries to unvalidate something without the right to do it");
+        $newresponse = $response->withJson(null,403);
     }
     return $newresponse;
 });
