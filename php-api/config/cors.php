32fd57d9798dc7918993facf3bf3fb98eab0100c
Phantase <phantase@free.fr>
2017-04-17 17:44:58 +0200

    Cors origin is now in configuration

--- php-api/config/cors.php
+++ php-api/config/cors.php
@@ -8,7 +8,7 @@ $app->options('/{routes:.+}', function ($request, $response, $args) {
 $app->add(function ($req, $res, $next) {
     $response = $next($req, $res);
     return $response
-            ->withHeader('Access-Control-Allow-Origin', 'http://localhost:8080')
+            ->withHeader('Access-Control-Allow-Origin', $this["settings"]["cors"]["origin"])
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
 });
--- php-api/configuration.template.php
+++ php-api/configuration.template.php
@@ -8,16 +8,19 @@ $config['db']['host']    = "";
 $config['db']['user']    = "";
 $config['db']['pass']    = "";
 $config['db']['dbname']  = "";
-$config['db']['charset'] = "utf8";
+$config['db']['charset'] = "utf8"; // e.g. utf8
 
 // Secret for JWT
 $config['jwt']['secret'] = "";
 
 // SMTP credential
-$config['smtp']['host']   = "";
-$config['smtp']['port']   = 587;
-$config['smtp']['user']   = "";
-$config['smtp']['pass']   = "";
-$config['smtp']['from']   = "";
-$config['smtp']['bcc']    = "";
-$config['smtp']['name']   = "";
+$config['smtp']['host']   = ""; // e.g. mail.aserver.com
+$config['smtp']['port']   = 587; // e.g. 587
+$config['smtp']['user']   = ""; // e.g. administrator@aserver.com
+$config['smtp']['pass']   = ""; // self explaining
+$config['smtp']['from']   = ""; // e.g. administrator@aserver.com
+$config['smtp']['bcc']    = ""; // e.g. registration@aserver.com
+$config['smtp']['name']   = ""; // e.g. Registration (A Server)
+
+// CORS
+$config['cors']['origin'] = ""; // e.g. http://someadressyouallow.com
