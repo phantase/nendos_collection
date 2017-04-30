45b262a1c0260262f66dab4ac14d02ae8bf5ba9b
Phantase <phantase@free.fr>
2017-04-30 14:41:13 +0200

    Add a component to visualize the name of a user with a link to his profile, just with his id

---
+++ vuejs-front/src/components/dblayouts/UserComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <span class="user">
+    <i class="fa fa-user-circle-o"></i> <router-link :to="'/profile/' + user.internalid" v-if="user">{{ user.username }}</router-link><span v-else>Unknown</span>
+  </span>
+</template>
+
+<script>
+import store from './../../store'
+import Vuex from 'vuex'
+
+export default {
+  name: 'UserComponent',
+  store: store,
+  props: ['userid'],
+  data () {
+    return {
+    }
+  },
+  computed: {
+    ...Vuex.mapGetters(['users']),
+    user () {
+      return this.users.find(user => user.internalid === this.userid)
+    }
+  },
+  destroyed () {
+    $('[role="tooltip"]').remove()
+  }
+}
+</script>
