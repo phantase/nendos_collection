982b4998b41bab3bf0f304a78c934bed8d574417
Phantase <phantase@free.fr>
2017-07-24 15:42:39 +0200

    Remove a conflict between user (filtered from users) and user (local user)

--- vuejs-front/src/components/User.vue
+++ vuejs-front/src/components/User.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="db-box" v-if="user">
+  <div class="db-box" v-if="userprofile">
 
     <div class="row">
       <div class="col-md-12">
@@ -7,18 +7,17 @@
           <div class="box-header with-border">
             <h3 class="box-title">
               <div class="pull-right">
-                <div><router-link :to="'/user/'+user.internalid+'/edit'" class="btn btn-xs bg-blue pull-right" v-if="canadmin || isself"><i class="fa fa-edit"></i> Edit</router-link></div>
+                <div><router-link :to="'/user/'+userprofile.internalid+'/edit'" class="btn btn-xs bg-blue pull-right" v-if="canadmin || isself"><i class="fa fa-edit"></i> Edit</router-link></div>
               </div>
               <div class="db-box-category text-yellow">
-                <span>{{ user.username }}</span>
+                <span>{{ userprofile.username }}</span>
               </div>
-              <div class="db-box-name" v-if="canadmin || isself">{{ user.usermail }}</div>
+              <div class="db-box-name" v-if="canadmin || isself">{{ userprofile.usermail }}</div>
             </h3>
           </div>
         </div>
       </div>
     </div>
-
     <div class="row">
       <div class="col-md-8 col-sm-12 col-xs-12">
         <div class="box">
@@ -27,43 +26,43 @@
             <ul class="list-group list-group-unbordered">
               <li class="list-group-item">
                 <b>Name</b>
-                <a class="pull-right">{{ user.username }}</a><br>
+                <a class="pull-right">{{ userprofile.username }}</a><br>
               </li>
               <li class="list-group-item" v-if="canadmin || isself">
                 <b>Mail</b>
-                <a class="pull-right">{{ user.usermail }}</a><br>
+                <a class="pull-right">{{ userprofile.usermail }}</a><br>
               </li>
               <li class="list-group-item">
                 <b>Roles</b>
                 <a class="pull-right">
-                  <i class="fa fa-gavel" data-toggle="tooltip" title="Administrator" v-if="user.administrator === '1'"></i>
-                  <i class="fa fa-check-square-o" data-toggle="tooltip" title="Validator" v-if="user.validator === '1'"></i>
-                  <i class="fa fa-pencil-square-o" data-toggle="tooltip" title="Editor" v-if="user.editor === '1'"></i>
+                  <i class="fa fa-gavel" data-toggle="tooltip" title="Administrator" v-if="userprofile.administrator === '1'"></i>
+                  <i class="fa fa-check-square-o" data-toggle="tooltip" title="Validator" v-if="userprofile.validator === '1'"></i>
+                  <i class="fa fa-pencil-square-o" data-toggle="tooltip" title="Editor" v-if="userprofile.editor === '1'"></i>
                   <i class="fa fa-user-circle-o" data-toggle="tooltip" title="User"></i>
                 </a><br>
               </li>
-              <li class="list-group-item" v-if="user.signupdate">
+              <li class="list-group-item" v-if="userprofile.signupdate">
                 <b>Sign-up date</b>
-                <a class="pull-right">{{ user.signupdate }}</a><br>
+                <a class="pull-right">{{ userprofile.signupdate }}</a><br>
               </li>
-              <li class="list-group-item" v-if="user.lastviewdate">
+              <li class="list-group-item" v-if="userprofile.lastviewdate">
                 <b>Last view date</b>
-                <a class="pull-right">{{ user.lastviewdate }}</a><br>
+                <a class="pull-right">{{ userprofile.lastviewdate }}</a><br>
               </li>
             </ul>
             <div class="pull-middle">
-                <router-link class="btn btn-warning" :to="'/user/'+user.internalid+'/collection'"><i class="fa fa-cube"></i> Collection</router-link>
-                <router-link class="btn btn-warning" :to="'/user/'+user.internalid+'/photos'"><i class="fa fa-image"></i> Photos</router-link>
-                <router-link class="btn btn-warning" :to="'/user/'+user.internalid+'/favorites'"><i class="fa fa-heart"></i> Favorites</router-link>
+                <router-link class="btn btn-warning" :to="'/user/'+userprofile.internalid+'/collection'"><i class="fa fa-cube"></i> Collection</router-link>
+                <router-link class="btn btn-warning" :to="'/user/'+userprofile.internalid+'/photos'"><i class="fa fa-image"></i> Photos</router-link>
+                <router-link class="btn btn-warning" :to="'/user/'+userprofile.internalid+'/favorites'"><i class="fa fa-heart"></i> Favorites</router-link>
             </div>
           </div>
         </div>
       </div>
       <div class="col-md-4 col-sm-12 col-xs-12">
         <div class="box">
-          <app-box-header title="Photo" collapsable="true" icon="fa-photo" :editable="canadmin || isself" :editlink="'/user/'+user.internalid+'/edit/image'"></app-box-header>
+          <app-box-header title="Photo" collapsable="true" icon="fa-photo" :editable="canadmin || isself" :editlink="'/user/'+userprofile.internalid+'/edit/image'"></app-box-header>
           <div class="box-body db-image">
-            <img :src="resources.img_url+'/images/users/'+user.internalid+'/thumb'" v-if="user.haspicture == '1'"/>
+            <img :src="resources.img_url+'/images/users/'+userprofile.internalid+'/thumb'" v-if="userprofile.haspicture == '1'"/>
             <img :src="resources.img_url+'/images/unknown'" v-else />
           </div>
         </div>
@@ -109,7 +108,7 @@ export default {
   },
   computed: {
     ...Vuex.mapGetters(['users', 'user', 'canadmin']),
-    user () {
+    userprofile () {
       return this.users.find(user => user.internalid === this.$route.params.id)
     },
     isself () {
@@ -118,8 +117,8 @@ export default {
   },
   beforeRouteEnter (to, from, next) {
     next(vm => {
-      if (vm.user) {
-        document.title += ' / ' + vm.user.username + ' / Id: ' + vm.user.internalid
+      if (vm.userprofile) {
+        document.title += ' / ' + vm.userprofile.username + ' / Id: ' + vm.userprofile.internalid
       } else {
         document.title += ' / Id: ' + vm.$route.params.id
       }
