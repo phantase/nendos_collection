368854755cffd7bf4a3a616f541e2195018974e9
Phantase <phantase@free.fr>
2018-03-13 21:47:32 +0100

    First test of models to see if it works (well... not really :'( )

---
+++ vuejs-front/src/models/accessory.ts
@@ -0,0 +1,31 @@
+export class Accessory {
+  internalid: number | undefined;
+  boxid: number | undefined;
+  nendoroidid: number | undefined;
+  type: string| undefined;
+  main_color: string | undefined;
+  other_color: string | undefined;
+  description: string | undefined;
+  creatorid: number | undefined;
+  creatorname: string | undefined;
+  creationdate: string | undefined;
+  editorid: number | undefined;
+  editorname: string | undefined;
+  editiondate: string | undefined;
+  validatorid: number | undefined;
+  validatorname: string | undefined;
+  validationdate: string | undefined;
+  haspicture: boolean | undefined;
+  xmin: number | undefined;
+  xmax: number | undefined;
+  ymin: number | undefined;
+  ymax: number | undefined;
+  photoannotationid: number | undefined;
+  colladdeddate: string | undefined;
+  collquantity: string | undefined;
+  colusers: any[]| undefined;
+  numberfavorited: number | undefined;
+  inuserfavorites: number | undefined;
+  favusers: any[] | undefined;
+  tags: any[] | undefined;
+}
